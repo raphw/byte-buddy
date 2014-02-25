70633e216fd16d7145d7af7cbba0d21696d46a75
rafwin <rafael.wth@web.de>
2014-02-25 18:15:56 +0100

    Completed end user API.

--- src/main/java/com/blogspot/mydailyjava/bytebuddy/ByteBuddy.java
+++ src/main/java/com/blogspot/mydailyjava/bytebuddy/ByteBuddy.java
@@ -6,12 +6,14 @@ import com.blogspot.mydailyjava.bytebuddy.dynamic.scaffold.FieldRegistry;
 import com.blogspot.mydailyjava.bytebuddy.dynamic.scaffold.MethodRegistry;
 import com.blogspot.mydailyjava.bytebuddy.dynamic.scaffold.subclass.ConstructorStrategy;
 import com.blogspot.mydailyjava.bytebuddy.dynamic.scaffold.subclass.SubclassDynamicTypeBuilder;
+import com.blogspot.mydailyjava.bytebuddy.instrumentation.Instrumentation;
 import com.blogspot.mydailyjava.bytebuddy.instrumentation.attribute.FieldAttributeAppender;
 import com.blogspot.mydailyjava.bytebuddy.instrumentation.attribute.MethodAttributeAppender;
 import com.blogspot.mydailyjava.bytebuddy.instrumentation.attribute.TypeAttributeAppender;
 import com.blogspot.mydailyjava.bytebuddy.instrumentation.method.matcher.MethodMatcher;
 import org.objectweb.asm.Opcodes;
 
+import java.lang.annotation.Annotation;
 import java.util.Collections;
 import java.util.List;
 
@@ -23,14 +25,193 @@ public class ByteBuddy {
     private static final String JAVA_VERSION = "java.version";
     private static final String BYTE_BUDDY_DEFAULT_PREFIX = "ByteBuddy";
 
-    private final ClassFormatVersion classFormatVersion;
-    private final NamingStrategy namingStrategy;
-    private final List<Class<?>> interfaceTypes;
-    private final MethodMatcher ignoredMethods;
-    private final ClassVisitorWrapper.Chain classVisitorWrapperChain;
-    private final MethodRegistry methodRegistry;
-    private final FieldAttributeAppender.Factory defaultFieldAttributeAppenderFactory;
-    private final MethodAttributeAppender.Factory defaultMethodAttributeAppenderFactory;
+    public static class MethodAnnotationTarget extends ByteBuddy {
+
+        protected final MethodMatcher methodMatcher;
+        protected final Instrumentation instrumentation;
+        protected final MethodAttributeAppender.Factory attributeAppenderFactory;
+
+        protected MethodAnnotationTarget(ClassFormatVersion classFormatVersion,
+                                         NamingStrategy namingStrategy,
+                                         List<Class<?>> interfaceTypes,
+                                         MethodMatcher ignoredMethods,
+                                         ClassVisitorWrapper.Chain classVisitorWrapperChain,
+                                         MethodRegistry methodRegistry,
+                                         FieldAttributeAppender.Factory defaultFieldAttributeAppenderFactory,
+                                         MethodAttributeAppender.Factory defaultMethodAttributeAppenderFactory,
+                                         MethodMatcher methodMatcher,
+                                         Instrumentation instrumentation,
+                                         MethodAttributeAppender.Factory attributeAppenderFactory) {
+            super(classFormatVersion,
+                    namingStrategy,
+                    interfaceTypes,
+                    ignoredMethods,
+                    classVisitorWrapperChain,
+                    methodRegistry,
+                    defaultFieldAttributeAppenderFactory,
+                    defaultMethodAttributeAppenderFactory);
+            this.methodMatcher = methodMatcher;
+            this.instrumentation = instrumentation;
+            this.attributeAppenderFactory = attributeAppenderFactory;
+        }
+
+        public MethodAnnotationTarget attribute(MethodAttributeAppender.Factory attributeAppenderFactory) {
+            return new MethodAnnotationTarget(classFormatVersion,
+                    namingStrategy,
+                    interfaceTypes,
+                    ignoredMethods,
+                    classVisitorWrapperChain,
+                    methodRegistry,
+                    defaultFieldAttributeAppenderFactory,
+                    defaultMethodAttributeAppenderFactory,
+                    methodMatcher,
+                    instrumentation,
+                    new MethodAttributeAppender.Factory.Compound(this.attributeAppenderFactory, attributeAppenderFactory));
+        }
+
+        public MethodAnnotationTarget annotateMethod(Annotation annotation) {
+            return attribute(new MethodAttributeAppender.ForAnnotation(annotation));
+        }
+
+        public MethodAnnotationTarget annotateParameter(int parameterIndex, Annotation annotation) {
+            return attribute(new MethodAttributeAppender.ForAnnotation(annotation, parameterIndex));
+        }
+
+        @Override
+        public ClassFormatVersion getClassFormatVersion() {
+            return materialize().getClassFormatVersion();
+        }
+
+        @Override
+        public NamingStrategy getNamingStrategy() {
+            return materialize().getNamingStrategy();
+        }
+
+        @Override
+        public List<Class<?>> getInterfaceTypes() {
+            return materialize().getInterfaceTypes();
+        }
+
+        @Override
+        public MethodMatcher getIgnoredMethods() {
+            return materialize().getIgnoredMethods();
+        }
+
+        @Override
+        public ClassVisitorWrapper.Chain getClassVisitorWrapperChain() {
+            return materialize().getClassVisitorWrapperChain();
+        }
+
+        @Override
+        public FieldAttributeAppender.Factory getDefaultFieldAttributeAppenderFactory() {
+            return materialize().getDefaultFieldAttributeAppenderFactory();
+        }
+
+        @Override
+        public MethodAttributeAppender.Factory getDefaultMethodAttributeAppenderFactory() {
+            return materialize().getDefaultMethodAttributeAppenderFactory();
+        }
+
+        @Override
+        public <T> DynamicType.Builder<T> subclass(Class<T> superType) {
+            return materialize().subclass(superType);
+        }
+
+        @Override
+        public <T> DynamicType.Builder<T> subclass(Class<T> superType, ConstructorStrategy constructorStrategy) {
+            return materialize().subclass(superType, constructorStrategy);
+        }
+
+        @Override
+        public ByteBuddy withClassFormatVersion(int classFormatVersion) {
+            return materialize().withClassFormatVersion(classFormatVersion);
+        }
+
+        @Override
+        public ByteBuddy withNamingStrategy(NamingStrategy namingStrategy) {
+            return materialize().withNamingStrategy(namingStrategy);
+        }
+
+        @Override
+        public ByteBuddy implementInterface(Class<?> type) {
+            return materialize().implementInterface(type);
+        }
+
+        @Override
+        public ByteBuddy ignoreMethods(MethodMatcher ignoredMethods) {
+            return materialize().ignoreMethods(ignoredMethods);
+        }
+
+        @Override
+        public ByteBuddy withClassVisitor(ClassVisitorWrapper classVisitorWrapper) {
+            return materialize().withClassVisitor(classVisitorWrapper);
+        }
+
+        @Override
+        public ByteBuddy withDefaultFieldAttributeAppender(FieldAttributeAppender.Factory attributeAppenderFactory) {
+            return materialize().withDefaultFieldAttributeAppender(attributeAppenderFactory);
+        }
+
+        @Override
+        public ByteBuddy withDefaultMethodAttributeAppender(MethodAttributeAppender.Factory attributeAppenderFactory) {
+            return materialize().withDefaultMethodAttributeAppender(attributeAppenderFactory);
+        }
+
+        @Override
+        public MatchedMethodInterception intercept(MethodMatcher methodMatcher) {
+            return materialize().intercept(methodMatcher);
+        }
+
+        protected ByteBuddy materialize() {
+            return new ByteBuddy(classFormatVersion,
+                    namingStrategy,
+                    interfaceTypes,
+                    ignoredMethods,
+                    classVisitorWrapperChain,
+                    methodRegistry.prepend(new MethodRegistry.LatentMethodMatcher.Simple(methodMatcher),
+                            instrumentation,
+                            attributeAppenderFactory),
+                    defaultFieldAttributeAppenderFactory,
+                    defaultMethodAttributeAppenderFactory);
+        }
+    }
+
+    public class MatchedMethodInterception {
+
+        protected final MethodMatcher methodMatcher;
+
+        public MatchedMethodInterception(MethodMatcher methodMatcher) {
+            this.methodMatcher = methodMatcher;
+        }
+
+
+        public MethodAnnotationTarget intercept(Instrumentation instrumentation) {
+            return new MethodAnnotationTarget(classFormatVersion,
+                    namingStrategy,
+                    interfaceTypes,
+                    ignoredMethods,
+                    classVisitorWrapperChain,
+                    methodRegistry,
+                    defaultFieldAttributeAppenderFactory,
+                    defaultMethodAttributeAppenderFactory,
+                    methodMatcher,
+                    instrumentation,
+                    MethodAttributeAppender.NoOp.INSTANCE);
+        }
+
+        public MethodAnnotationTarget withoutCode() {
+            return intercept(Instrumentation.ForAbstractMethod.INSTANCE);
+        }
+    }
+
+    protected final ClassFormatVersion classFormatVersion;
+    protected final NamingStrategy namingStrategy;
+    protected final List<Class<?>> interfaceTypes;
+    protected final MethodMatcher ignoredMethods;
+    protected final ClassVisitorWrapper.Chain classVisitorWrapperChain;
+    protected final MethodRegistry methodRegistry;
+    protected final FieldAttributeAppender.Factory defaultFieldAttributeAppenderFactory;
+    protected final MethodAttributeAppender.Factory defaultMethodAttributeAppenderFactory;
 
     public ByteBuddy() {
         classFormatVersion = ClassFormatVersion.forJavaVersion(Integer.parseInt(System.getProperty(JAVA_VERSION)));
@@ -186,7 +367,7 @@ public class ByteBuddy {
                 nonNull(attributeAppenderFactory));
     }
 
-    public void intercept(MethodMatcher methodMatcher) {
-        // TODO
+    public MatchedMethodInterception intercept(MethodMatcher methodMatcher) {
+        return new MatchedMethodInterception(methodMatcher);
     }
 }
