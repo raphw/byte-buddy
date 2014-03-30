d3fc378e319836d96a09ed88a55e0e79e6717f6e
rafwin <rafael.wth@web.de>
2014-03-30 03:04:51 +0200

    Fixed several styling issues but broke navigation.

--- js/bootply.js
+++ js/bootply.js
@@ -1,27 +1,14 @@
 $(document).ready(function () {
-
-    $('#sidebar').affix({});
-
-    var $body = $(document.body);
-    var navHeight = $('.navbar').outerHeight(true) + 60;
-
-    $body.scrollspy({
-        target: '#leftCol',
-        offset: navHeight
-    });
-
-    /* smooth scrolling sections */
     $('a[href*=#]:not([href=#])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
             if (target.length) {
                 $('html,body').animate({
-                    scrollTop: target.offset().top - 60
+                    scrollTop: target.offset().top - 100
                 }, 1000);
                 return false;
             }
         }
     });
-
 });
