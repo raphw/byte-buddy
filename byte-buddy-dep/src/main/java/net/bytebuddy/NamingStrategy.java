3409c669b5184f79048c89d3874f1ca83d7cf3b7
rafwin <rafael.wth@web.de>
2014-06-07 21:27:54 +0200

    Added another naming strategy. Fixed suffixing random naming strategy to apply renaming for any class in the java package instead of only the java.lang package.

--- byte-buddy-dep/src/main/java/net/bytebuddy/NamingStrategy.java
+++ byte-buddy-dep/src/main/java/net/bytebuddy/NamingStrategy.java
@@ -9,7 +9,7 @@ import java.util.Collection;
 import java.util.Random;
 
 /**
- * A naming strategy for finding a fully qualified internalName for a Java type.
+ * A naming strategy for finding a fully qualified name for a Java type.
  * <p>&nbsp;</p>
  * Note that subclasses that lie within the same package as their superclass have improved access to overriding
  * package-private methods of their super type.
@@ -17,10 +17,10 @@ import java.util.Random;
 public interface NamingStrategy {
 
     /**
-     * Generates a fully qualified internalName for a Java type. The resulting should not lie within the {@code java.lang}
-     * package since such types cannot be loaded using a normal class loader. Also, the internalName should not yet be taken
-     * by another type since this would cause conflicts in the internalName space. Therefore, it is recommendable to include
-     * a random sequence within the internalName.
+     * Generates a fully qualified name for a Java type. The resulting should not lie within the {@code java.lang}
+     * package since such types cannot be loaded using a normal class loader. Also, the name should not yet be taken
+     * by another type since this would cause conflicts in the name space. Therefore, it is recommendable to include
+     * a random sequence within the name.
      *
      * @param unnamedType An unnamed type that is to be named.
      * @return A valid identifier for a Java type.
@@ -76,25 +76,27 @@ public interface NamingStrategy {
     }
 
     /**
-     * A naming strategy that creates a internalName by concatenating
+     * A naming strategy that creates a name by concatenating:
      * <ol>
-     * <li>The super classes package and internalName</li>
+     * <li>The super classes package and name</li>
      * <li>A given suffix string</li>
      * <li>A random number</li>
      * </ol>
-     * Between all these elements, a {@code $} sign is included into the internalName to improve readability.
+     * Between all these elements, a {@code $} sign is included into the name to improve readability. As an exception,
+     * types that subclass classes from the {@code java.**} packages are prefixed with a given package. This is
+     * necessary as it is illegal to define non-bootstrap classes in this name space.
      */
     static class SuffixingRandom implements NamingStrategy {
 
         /**
-         * The package prefix of the {@code java.lang} package for which the definition of non-bootstrap
-         * types is illegal.
+         * The package prefix of the {@code java.**} packages for which the definition of non-bootstrap types is
+         * illegal.
          */
-        private static final String JAVA_LANG_PACKAGE = "java.lang.";
+        private static final String JAVA_PACKAGE = "java.";
 
         /**
          * The default package for defining types that are renamed to not be contained in the
-         * {@link net.bytebuddy.NamingStrategy.SuffixingRandom#JAVA_LANG_PACKAGE} package.
+         * {@link net.bytebuddy.NamingStrategy.SuffixingRandom#JAVA_PACKAGE} package.
          */
         private static final String BYTE_BUDDY_RENAME_PACKAGE = "net.bytebuddy.renamed";
 
@@ -104,7 +106,7 @@ public interface NamingStrategy {
         private final String suffix;
 
         /**
-         * The renaming location for types of the {@link net.bytebuddy.NamingStrategy.SuffixingRandom#JAVA_LANG_PACKAGE}.
+         * The renaming location for types of the {@link net.bytebuddy.NamingStrategy.SuffixingRandom#JAVA_PACKAGE}.
          */
         private final String javaLangPackagePrefix;
 
@@ -140,10 +142,10 @@ public interface NamingStrategy {
         @Override
         public String name(UnnamedType unnamedType) {
             String superClassName = unnamedType.getSuperClass().getName();
-            if (superClassName.startsWith(JAVA_LANG_PACKAGE)) {
+            if (superClassName.startsWith(JAVA_PACKAGE)) {
                 superClassName = javaLangPackagePrefix + "." + superClassName;
             }
-            return String.format("%s$$%s$$%d", superClassName, suffix, Math.abs(random.nextInt()));
+            return String.format("%s$%s$%d", superClassName, suffix, Math.abs(random.nextInt()));
         }
 
         @Override
@@ -173,7 +175,58 @@ public interface NamingStrategy {
     }
 
     /**
-     * A naming strategy that applies a fixed internalName.
+     * A naming strategy that creates a name by prefixing a given class and its package with another package and
+     * by appending a random number to the class's simple name.
+     */
+    static class PrefixingRandom implements NamingStrategy {
+
+        /**
+         * The package to prefix.
+         */
+        private final String prefix;
+
+        /**
+         * A random number generator.
+         */
+        private final Random random;
+
+        /**
+         * Creates a new prefixing random naming strategy.
+         *
+         * @param prefix The prefix to append.
+         */
+        public PrefixingRandom(String prefix) {
+            this.prefix = prefix;
+            random = new Random();
+        }
+
+        @Override
+        public String name(UnnamedType unnamedType) {
+            return String.format("%s.%s$%d", prefix, unnamedType.getSuperClass().getName(), Math.abs(random.nextInt()));
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return this == other || !(other == null || getClass() != other.getClass())
+                    && prefix.equals(((PrefixingRandom) other).prefix);
+        }
+
+        @Override
+        public int hashCode() {
+            return prefix.hashCode();
+        }
+
+        @Override
+        public String toString() {
+            return "NamingStrategy.PrefixingRandom{" +
+                    "prefix='" + prefix + '\'' +
+                    ", random=" + random +
+                    '}';
+        }
+    }
+
+    /**
+     * A naming strategy that applies a fixed name.
      * <p>&nbsp;</p>
      * This strategy should only be used for one shot type generators since they will otherwise create types that
      * impose naming conflicts.
@@ -188,7 +241,7 @@ public interface NamingStrategy {
         /**
          * Creates an immutable fixed naming strategy.
          *
-         * @param name The internalName for the created type.
+         * @param name The name for the created type.
          */
         public Fixed(String name) {
             this.name = name;
--- byte-buddy-dep/src/test/java/net/bytebuddy/NamingStrategyTest.java
+++ byte-buddy-dep/src/test/java/net/bytebuddy/NamingStrategyTest.java
@@ -26,25 +26,25 @@ public class NamingStrategyTest {
     private NamingStrategy.UnnamedType unnamedType;
 
     @Test
-    public void testPrefixingRandomNonConflictingPackage() throws Exception {
+    public void testSuffixingRandomNonConflictingPackage() throws Exception {
         when(unnamedType.getSuperClass()).thenReturn(new TypeDescription.ForLoadedType(MethodVisitor.class));
         NamingStrategy namingStrategy = new NamingStrategy.SuffixingRandom(FOO);
-        assertThat(namingStrategy.name(unnamedType), startsWith(MethodVisitor.class.getName() + "$$" + FOO + "$$"));
+        assertThat(namingStrategy.name(unnamedType), startsWith(MethodVisitor.class.getName() + "$" + FOO + "$"));
         verify(unnamedType, atLeast(1)).getSuperClass();
         verifyNoMoreInteractions(unnamedType);
     }
 
     @Test
-    public void testPrefixingRandomConflictingPackage() throws Exception {
+    public void testSuffixingRandomConflictingPackage() throws Exception {
         when(unnamedType.getSuperClass()).thenReturn(new TypeDescription.ForLoadedType(Object.class));
         NamingStrategy namingStrategy = new NamingStrategy.SuffixingRandom(FOO, BAR);
-        assertThat(namingStrategy.name(unnamedType), startsWith(BAR + "." + Object.class.getName() + "$$" + FOO + "$$"));
+        assertThat(namingStrategy.name(unnamedType), startsWith(BAR + "." + Object.class.getName() + "$" + FOO + "$"));
         verify(unnamedType, atLeast(1)).getSuperClass();
         verifyNoMoreInteractions(unnamedType);
     }
 
     @Test
-    public void testPrefixingRandomEqualsHashCode() throws Exception {
+    public void testSuffixingRandomEqualsHashCode() throws Exception {
         assertThat(new NamingStrategy.SuffixingRandom(FOO, BAR).hashCode(), is(new NamingStrategy.SuffixingRandom(FOO, BAR).hashCode()));
         assertThat(new NamingStrategy.SuffixingRandom(FOO, BAR), equalTo(new NamingStrategy.SuffixingRandom(FOO, BAR)));
         assertThat(new NamingStrategy.SuffixingRandom(FOO, BAR).hashCode(), not(is(new NamingStrategy.SuffixingRandom(BAR, FOO).hashCode())));
@@ -59,10 +59,27 @@ public class NamingStrategyTest {
     }
 
     @Test
-    public void testFixedRandomEqualsHashCode() throws Exception {
+    public void testFixedEqualsHashCode() throws Exception {
         assertThat(new NamingStrategy.Fixed(FOO).hashCode(), is(new NamingStrategy.Fixed(FOO).hashCode()));
         assertThat(new NamingStrategy.Fixed(FOO), equalTo(new NamingStrategy.Fixed(FOO)));
         assertThat(new NamingStrategy.Fixed(FOO).hashCode(), not(is(new NamingStrategy.Fixed(BAR).hashCode())));
         assertThat(new NamingStrategy.Fixed(FOO), not(equalTo(new NamingStrategy.Fixed(BAR))));
+    }
+
+    @Test
+    public void testPrefixingRandom() throws Exception {
+        when(unnamedType.getSuperClass()).thenReturn(new TypeDescription.ForLoadedType(Object.class));
+        NamingStrategy namingStrategy = new NamingStrategy.PrefixingRandom(FOO);
+        assertThat(namingStrategy.name(unnamedType), startsWith(FOO + "." + Object.class.getName()));
+        verify(unnamedType).getSuperClass();
+        verifyNoMoreInteractions(unnamedType);
+    }
+
+    @Test
+    public void testPrefixingRandomEqualsHashCode() throws Exception {
+        assertThat(new NamingStrategy.PrefixingRandom(FOO).hashCode(), is(new NamingStrategy.PrefixingRandom(FOO).hashCode()));
+        assertThat(new NamingStrategy.PrefixingRandom(FOO), equalTo(new NamingStrategy.PrefixingRandom(FOO)));
+        assertThat(new NamingStrategy.PrefixingRandom(FOO).hashCode(), not(is(new NamingStrategy.PrefixingRandom(BAR).hashCode())));
+        assertThat(new NamingStrategy.PrefixingRandom(FOO), not(equalTo(new NamingStrategy.PrefixingRandom(BAR))));
     }
 }
