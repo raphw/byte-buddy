12b475f6594dafb257c7bab7ecbb31929bd4467d
rafwin <rafael.wth@web.de>
2014-06-20 10:40:08 +0200

    Upgraded to version 0.2.1.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ angular.module('byteBuddy', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.affix', 'd
     .constant('repository', {
         groupId: 'net.bytebuddy',
         artifactId: 'byte-buddy',
-        version: '0.2'
+        version: '0.2.1'
     })
 
     .config(function ($routeProvider) {
