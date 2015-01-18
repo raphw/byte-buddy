d50aaa7654be2f1a027f1ad83c19e497a4808afb
Rafael Winterhalter <rafael.wth@web.de>
2015-01-18 18:22:32 +0100

    Added information on version 0.5.2.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ angular.module('byteBuddy', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.affix', 'd
     .constant('repository', {
         groupId: 'net.bytebuddy',
         artifactId: 'byte-buddy',
-        version: '0.5.1'
+        version: '0.5.2'
     })
 
     .config(function ($routeProvider) {
