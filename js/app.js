2b13ff18fe833d6190c22a03447eef7be1af99fb
Rafael Winterhalter <rafael.wth@web.de>
2015-05-06 11:54:14 +0200

    Release of version 0.6.4.

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ angular.module('byteBuddy', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.affix', 'd
     .constant('repository', {
         groupId: 'net.bytebuddy',
         artifactId: 'byte-buddy',
-        version: '0.6.3'
+        version: '0.6.4'
     })
 
     .config(function ($routeProvider) {
