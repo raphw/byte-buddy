cbda9f81b8f9e7c8d2a40dc6bca8491a8a6b955d
Rafael Winterhalter <rafael.wth@web.de>
2015-06-01 12:18:06 +0200

    Upgraded to version 0.6.7

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ angular.module('byteBuddy', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.affix', 'd
     .constant('repository', {
         groupId: 'net.bytebuddy',
         artifactId: 'byte-buddy',
-        version: '0.6.6'
+        version: '0.6.7'
     })
 
     .config(function ($routeProvider) {
