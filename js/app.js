0249f590734d12664018d0f1ffe9905acbe7476f
Rafael Winterhalter <rafael.wth@web.de>
2015-07-15 13:27:23 +0200

    Changed default version to 0.6.12

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ angular.module('byteBuddy', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.affix', 'd
     .constant('repository', {
         groupId: 'net.bytebuddy',
         artifactId: 'byte-buddy',
-        version: '0.7'
+        version: '0.6.12'
     })
 
     .config(function ($routeProvider) {
