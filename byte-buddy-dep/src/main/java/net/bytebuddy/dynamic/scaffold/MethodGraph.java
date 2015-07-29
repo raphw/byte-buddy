ba8f29e06e4d0893984bef8af8ea8ebf5a261ec8
Rafael Winterhalter <rafael.wth@web.de>
2015-07-29 09:10:56 +0200

    Added key collection to method graph.

--- byte-buddy-dep/src/main/java/net/bytebuddy/dynamic/scaffold/MethodGraph.java
+++ byte-buddy-dep/src/main/java/net/bytebuddy/dynamic/scaffold/MethodGraph.java
@@ -1,7 +1,6 @@
 package net.bytebuddy.dynamic.scaffold;
 
 import net.bytebuddy.description.method.MethodDescription;
-import net.bytebuddy.description.method.ParameterDescription;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.description.type.generic.GenericTypeDescription;
 
@@ -9,20 +8,7 @@ import java.util.*;
 
 public interface MethodGraph {
 
-    Node find(MethodDescription.Token methodToken);
-
-    List<Node> listNodes();
-
-    MethodGraph getSuperGraph();
-
-    Map<GenericTypeDescription, MethodGraph> getInterfaceGraphs();
-
-    interface Node {
-
-        MethodDescription getRepresentative();
-
-        Set<MethodDescription.Token> getBridges();
-    }
+    MethodDescription locate(MethodDescription.Token methodToken);
 
     interface Factory {
 
@@ -30,100 +16,33 @@ public interface MethodGraph {
 
         class Default implements Factory {
 
-            public static Factory forJavaLanguage() {
-                return new Default(KeyFactory.ForJavaLanguage.INSTANCE);
-            }
-
-            private final KeyFactory<?> keyFactory;
-
-            public Default(KeyFactory<?> keyFactory) {
-                this.keyFactory = keyFactory;
-            }
-
             @Override
             public MethodGraph make(TypeDescription typeDescription) {
                 return null;
             }
 
-            public interface KeyFactory<T> {
+            protected static class KeyCollection {
 
-                T make(MethodDescription.Token methodToken);
+                private final String internalName;
 
-                enum ForJavaLanguage implements KeyFactory<ForJavaLanguage.Key> {
+                private final Set<MethodDescription.Token> keys;
 
-                    INSTANCE;
+                protected KeyCollection(String internalName, Set<MethodDescription.Token> keys) {
+                    this.internalName = internalName;
+                    this.keys = keys;
+                }
 
-                    @Override
-                    public Key make(MethodDescription.Token methodToken) {
-                        List<ParameterDescription.Token> parameterTokens = methodToken.getParameterTokens();
-                        List<TypeDescription> rawParameterTypes = new ArrayList<TypeDescription>(parameterTokens.size());
-                        for (ParameterDescription.Token parameterToken : parameterTokens) {
-                            rawParameterTypes.add(parameterToken.getType().asRawType());
-                        }
-                        return new Key(methodToken.getInternalName(), rawParameterTypes);
-                    }
+                @Override
+                public boolean equals(Object other) {
+                    return other == this || other instanceof KeyCollection
+                            && !Collections.disjoint(keys, ((KeyCollection) other).keys);
+                }
 
-                    public static class Key {
-
-                        private final String internalName;
-
-                        private final List<? extends TypeDescription> rawParameterTypes;
-
-                        protected Key(String internalName, List<? extends TypeDescription> rawParameterTypes) {
-                            this.internalName = internalName;
-                            this.rawParameterTypes = rawParameterTypes;
-                        }
-
-                        @Override
-                        public boolean equals(Object other) {
-                            if (this == other) return true;
-                            if (other == null || getClass() != other.getClass()) return false;
-                            Key key = (Key) other;
-                            return internalName.equals(key.internalName)
-                                    && rawParameterTypes.equals(key.rawParameterTypes);
-                        }
-
-                        @Override
-                        public int hashCode() {
-                            int result = internalName.hashCode();
-                            result = 31 * result + rawParameterTypes.hashCode();
-                            return result;
-                        }
-                    }
+                @Override
+                public int hashCode() {
+                    return internalName.hashCode();
                 }
             }
-        }
-    }
-
-    class Simple implements MethodGraph {
-
-        private final MethodGraph superGraph;
-
-        private final Map<GenericTypeDescription, MethodGraph> interfaceGraphs;
-
-        protected Simple(MethodGraph superGraph, Map<GenericTypeDescription, MethodGraph> interfaceGraphs) {
-            this.superGraph = superGraph;
-            this.interfaceGraphs = interfaceGraphs;
-        }
-
-        @Override
-        public Node find(MethodDescription.Token methodToken) {
-            return null;
-        }
-
-        @Override
-        public List<Node> listNodes() {
-            return null;
-        }
-
-        @Override
-        public MethodGraph getSuperGraph() {
-            return superGraph;
-        }
-
-        @Override
-        public Map<GenericTypeDescription, MethodGraph> getInterfaceGraphs() {
-            return interfaceGraphs;
         }
     }
 }
