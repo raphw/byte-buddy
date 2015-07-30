cbe3fadc44db5c0bbe7650a928602a513dd1e358
Rafael Winterhalter <rafael.wth@web.de>
2015-07-30 17:10:15 +0200

    Fixed test that did not compile after refactoring.

--- byte-buddy-dep/src/test/java/net/bytebuddy/dynamic/scaffold/MethodGraphCompilerDefaultKeyTest.java
+++ byte-buddy-dep/src/test/java/net/bytebuddy/dynamic/scaffold/MethodGraphCompilerDefaultKeyTest.java
@@ -1,6 +1,5 @@
 package net.bytebuddy.dynamic.scaffold;
 
-import net.bytebuddy.description.method.MethodDescription;
 import net.bytebuddy.test.utility.MockitoRule;
 import net.bytebuddy.test.utility.ObjectPropertyAssertion;
 import org.junit.Rule;
@@ -8,10 +7,7 @@ import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.mockito.Mock;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
@@ -27,78 +23,78 @@ public class MethodGraphCompilerDefaultKeyTest {
 
     @Mock
     private SampleKey foo, bar, qux;
-//
-//    @Test
-//    public void testEqualsSimilar() throws Exception {
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo)).hashCode(),
-//                is(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo)).hashCode()));
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo)),
-//                is(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo))));
-//    }
-//
-//    @Test
-//    public void testNotEqualsDifferentName() throws Exception {
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo)).hashCode(),
-//                not(new MethodGraph.Compiler.Default.Key<SampleKey>(BAR, Collections.singleton(foo)).hashCode()));
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo)),
-//                not(new MethodGraph.Compiler.Default.Key<SampleKey>(BAR, Collections.singleton(foo))));
-//    }
-//
-//    @Test
-//    public void testNotEqualDifferentToken() throws Exception {
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo)).hashCode(),
-//                is(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(bar)).hashCode()));
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo)),
-//                not(new MethodGraph.Compiler.Default.Key<SampleKey>(BAR, Collections.singleton(bar))));
-//    }
-//
-//    @Test
-//    public void testEqualsSuperSet() throws Exception {
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar))).hashCode(),
-//                is(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(bar)).hashCode()));
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar))),
-//                is(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo))));
-//    }
-//
-//    @Test
-//    public void testEqualsSubSet() throws Exception {
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo)).hashCode(),
-//                is(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar))).hashCode()));
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(foo)),
-//                is(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar)))));
-//    }
-//
-//    @Test
-//    public void testNotEqualsDistinctSet() throws Exception {
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar))).hashCode(),
-//                is(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(qux)).hashCode()));
-//        assertThat(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar))),
-//                not(new MethodGraph.Compiler.Default.Key<SampleKey>(FOO, Collections.singleton(qux))));
-//    }
 
-//    @Test(expected = IllegalStateException.class)
-//    @SuppressWarnings("unchecked")
-//    public void testInitialEntryCannotBeMerged() throws Exception {
-//        new MethodGraph.Compiler.Default.Key.Store.Entry.Initial(new MethodGraph.Compiler.Default.Key
-//                .Identifying(FOO, Collections.singleton(mock(SampleKey.class))))
-//                .mergeWith(mock(MethodGraph.Compiler.Default.Key.Identifying.class));
-//    }
-//
-//    @Test(expected = IllegalStateException.class)
-//    @SuppressWarnings("unchecked")
-//    public void testInitialEntryCannotBeTransformed() throws Exception {
-//        new MethodGraph.Compiler.Default.Key.Store.Entry.Initial(new MethodGraph.Compiler.Default.Key
-//                .Identifying(FOO, Collections.singleton(mock(SampleKey.class))))
-//                .asNode();
-//    }
-//
-//    @Test(expected = IllegalStateException.class)
-//    @SuppressWarnings("unchecked")
-//    public void testInitialEntryCannotExposeKey() throws Exception {
-//        new MethodGraph.Compiler.Default.Key.Store.Entry.Initial(new MethodGraph.Compiler.Default.Key
-//                .Identifying(FOO, Collections.singleton(mock(SampleKey.class))))
-//                .getKey();
-//    }
+    @Test
+    public void testEqualsSimilar() throws Exception {
+        assertThat(new PseudoKey(FOO, Collections.singleton(foo)).hashCode(),
+                is(new PseudoKey(FOO, Collections.singleton(foo)).hashCode()));
+        assertThat(new PseudoKey(FOO, Collections.singleton(foo)),
+                is(new PseudoKey(FOO, Collections.singleton(foo))));
+    }
+
+    @Test
+    public void testNotEqualsDifferentName() throws Exception {
+        assertThat(new PseudoKey(FOO, Collections.singleton(foo)).hashCode(),
+                not(new PseudoKey(BAR, Collections.singleton(foo)).hashCode()));
+        assertThat(new PseudoKey(FOO, Collections.singleton(foo)),
+                not(new PseudoKey(BAR, Collections.singleton(foo))));
+    }
+
+    @Test
+    public void testNotEqualDifferentToken() throws Exception {
+        assertThat(new PseudoKey(FOO, Collections.singleton(foo)).hashCode(),
+                is(new PseudoKey(FOO, Collections.singleton(bar)).hashCode()));
+        assertThat(new PseudoKey(FOO, Collections.singleton(foo)),
+                not(new PseudoKey(BAR, Collections.singleton(bar))));
+    }
+
+    @Test
+    public void testEqualsSuperSet() throws Exception {
+        assertThat(new PseudoKey(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar))).hashCode(),
+                is(new PseudoKey(FOO, Collections.singleton(bar)).hashCode()));
+        assertThat(new PseudoKey(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar))),
+                is(new PseudoKey(FOO, Collections.singleton(foo))));
+    }
+
+    @Test
+    public void testEqualsSubSet() throws Exception {
+        assertThat(new PseudoKey(FOO, Collections.singleton(foo)).hashCode(),
+                is(new PseudoKey(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar))).hashCode()));
+        assertThat(new PseudoKey(FOO, Collections.singleton(foo)),
+                is(new PseudoKey(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar)))));
+    }
+
+    @Test
+    public void testNotEqualsDistinctSet() throws Exception {
+        assertThat(new PseudoKey(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar))).hashCode(),
+                is(new PseudoKey(FOO, Collections.singleton(qux)).hashCode()));
+        assertThat(new PseudoKey(FOO, new HashSet<SampleKey>(Arrays.asList(foo, bar))),
+                not(new PseudoKey(FOO, Collections.singleton(qux))));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    @SuppressWarnings("unchecked")
+    public void testInitialEntryCannotBeMerged() throws Exception {
+        new MethodGraph.Compiler.Default.Key.Store.Entry.Initial(new MethodGraph.Compiler.Default.Key
+                .Identifying(FOO, Collections.emptyMap()))
+                .mergeWith(mock(MethodGraph.Compiler.Default.Key.Identifying.class));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    @SuppressWarnings("unchecked")
+    public void testInitialEntryCannotBeTransformed() throws Exception {
+        new MethodGraph.Compiler.Default.Key.Store.Entry.Initial(new MethodGraph.Compiler.Default.Key
+                .Identifying(FOO, Collections.emptyMap()))
+                .asNode();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    @SuppressWarnings("unchecked")
+    public void testInitialEntryCannotExposeKey() throws Exception {
+        new MethodGraph.Compiler.Default.Key.Store.Entry.Initial(new MethodGraph.Compiler.Default.Key
+                .Identifying(FOO, Collections.emptyMap()))
+                .getKey();
+    }
 
     @Test
     public void testKeyObjectProperties() throws Exception {
@@ -126,6 +122,21 @@ public class MethodGraphCompilerDefaultKeyTest {
         ObjectPropertyAssertion.of(MethodGraph.Compiler.Default.Key.Store.Entry.Ambiguous.Node.class).apply();
         ObjectPropertyAssertion.of(MethodGraph.Compiler.Default.Key.Store.Entry.ForMethod.class).apply();
         ObjectPropertyAssertion.of(MethodGraph.Compiler.Default.Key.Store.Entry.ForMethod.Node.class).apply();
+    }
+    
+    protected static class PseudoKey extends MethodGraph.Compiler.Default.Key<SampleKey> {
+
+        private final Set<SampleKey> identifiers;
+
+        public PseudoKey(String internalName, Set<SampleKey> identifiers) {
+            super(internalName);
+            this.identifiers = identifiers;
+        }
+
+        @Override
+        protected Set<SampleKey> getIdentifiers() {
+            return identifiers;
+        }
     }
     
     public static class SampleKey {
