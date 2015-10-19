55fd40db68539ad8d711ae4e1115c4356e59e01c
rgoldberg <ross.goldberg@gmail.com>
2015-10-19 08:23:30 -0400

    Fixed net.bytebuddy.implementation.attribute.AnnotationAppender.ValueFilter.SkipDefaults#sRelevant(...)

--- byte-buddy-dep/src/test/java/net/bytebuddy/implementation/attribute/AnnotationAppenderValueFilterSkipDefaultsTest.java
+++ byte-buddy-dep/src/test/java/net/bytebuddy/implementation/attribute/AnnotationAppenderValueFilterSkipDefaultsTest.java
@@ -26,7 +26,7 @@ public class AnnotationAppenderValueFilterSkipDefaultsTest {
 
     @Test
     public void testFilteringNoDefault() throws Exception {
-        assertThat(AnnotationAppender.ValueFilter.SkipDefaults.INSTANCE.isRelevant(annotationDescription, methodDescription), is(false));
+        assertThat(AnnotationAppender.ValueFilter.SkipDefaults.INSTANCE.isRelevant(annotationDescription, methodDescription), is(true));
         verifyZeroInteractions(annotationDescription);
         verify(methodDescription).getDefaultValue();
         verifyNoMoreInteractions(methodDescription);
@@ -36,7 +36,7 @@ public class AnnotationAppenderValueFilterSkipDefaultsTest {
     public void testFilteringNoEquality() throws Exception {
         when(methodDescription.getDefaultValue()).thenReturn(new Object());
         when(annotationDescription.getValue(methodDescription)).thenReturn(new Object());
-        assertThat(AnnotationAppender.ValueFilter.SkipDefaults.INSTANCE.isRelevant(annotationDescription, methodDescription), is(false));
+        assertThat(AnnotationAppender.ValueFilter.SkipDefaults.INSTANCE.isRelevant(annotationDescription, methodDescription), is(true));
         verify(annotationDescription).getValue(methodDescription);
         verifyNoMoreInteractions(annotationDescription);
         verify(methodDescription).getDefaultValue();
@@ -48,7 +48,7 @@ public class AnnotationAppenderValueFilterSkipDefaultsTest {
         Object value = new Object();
         when(methodDescription.getDefaultValue()).thenReturn(value);
         when(annotationDescription.getValue(methodDescription)).thenReturn(value);
-        assertThat(AnnotationAppender.ValueFilter.SkipDefaults.INSTANCE.isRelevant(annotationDescription, methodDescription), is(true));
+        assertThat(AnnotationAppender.ValueFilter.SkipDefaults.INSTANCE.isRelevant(annotationDescription, methodDescription), is(false));
         verify(annotationDescription).getValue(methodDescription);
         verifyNoMoreInteractions(annotationDescription);
         verify(methodDescription).getDefaultValue();
