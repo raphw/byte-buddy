5ddf4b960251afe5bc935666bcad3171cc11e0fc
Rafael Winterhalter <rafael.wth@web.de>
2015-12-11 10:52:46 +0100

    Added missing documentation.

--- byte-buddy-dep/src/main/java/net/bytebuddy/utility/JavaType.java
+++ byte-buddy-dep/src/main/java/net/bytebuddy/utility/JavaType.java
@@ -49,6 +49,7 @@ public enum JavaType {
      *
      * @param typeName   The binary name of this type.
      * @param modifiers  The modifiers of this type when creating a stub.
+     * @param superType  The super class of this type when creating a stub.
      * @param interfaces The interfaces of this type when creating a stub.
      */
     JavaType(String typeName, int modifiers, Class<?> superType, Class<?>... interfaces) {
