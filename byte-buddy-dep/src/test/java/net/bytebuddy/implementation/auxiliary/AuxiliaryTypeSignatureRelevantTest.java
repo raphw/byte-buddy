d6e9aa164d46a437974fbfa846490b51a4cc317a
Rafael Winterhalter <rafael.wth@gmail.com>
2016-01-04 21:51:49 +0100

    Added additional test.

---
+++ byte-buddy-dep/src/test/java/net/bytebuddy/implementation/auxiliary/AuxiliaryTypeSignatureRelevantTest.java
@@ -0,0 +1,30 @@
+package net.bytebuddy.implementation.auxiliary;
+
+import org.junit.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Modifier;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class AuxiliaryTypeSignatureRelevantTest {
+
+    @Test
+    public void testSourceRentention() throws Exception {
+        assertThat(AuxiliaryType.SignatureRelevant.class.getAnnotation(Retention.class).value(), is(RetentionPolicy.SOURCE));
+    }
+
+    @Test
+    public void testTypeTarget() throws Exception {
+        assertThat(AuxiliaryType.SignatureRelevant.class.getAnnotation(Target.class).value(), is(new ElementType[]{ElementType.TYPE}));
+    }
+
+    @Test
+    public void testModifiers() throws Exception {
+        assertThat(Modifier.isPublic(AuxiliaryType.SignatureRelevant.class.getModifiers()), is(true));
+    }
+}
