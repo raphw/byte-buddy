18ae795dd817d616afba2409ea04292cdc0d7310
Rafael Winterhalter <rafael.wth@web.de>
2016-02-05 23:32:50 +0100

    Update to 1.2.0

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ angular.module('byteBuddy', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.affix', 'd
     .constant('repository', {
         groupId: 'net.bytebuddy',
         artifactId: 'byte-buddy',
-        version: '1.1.1'
+        version: '1.2.0'
     })
 
     .config(function ($routeProvider) {
