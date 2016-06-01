c6f4bcd883ba28d36b3005e7e61ae41306a13343
Rafael Winterhalter <rafael.wth@gmail.com>
2016-06-01 15:48:37 +0200

    Add missing javadoc.

--- byte-buddy-dep/src/main/java/net/bytebuddy/matcher/IsNamedMatcher.java
+++ byte-buddy-dep/src/main/java/net/bytebuddy/matcher/IsNamedMatcher.java
@@ -2,6 +2,11 @@ package net.bytebuddy.matcher;
 
 import net.bytebuddy.description.NamedElement;
 
+/**
+ * An element matcher that matches a named element only if is explicitly named.
+ *
+ * @param <T> The type of the matched entity.
+ */
 public class IsNamedMatcher<T extends NamedElement.WithOptionalName> extends ElementMatcher.Junction.AbstractBase<T> {
 
     @Override
