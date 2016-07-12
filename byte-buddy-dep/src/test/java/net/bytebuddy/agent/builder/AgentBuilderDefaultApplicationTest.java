7737d70441d1263f8397380244d41a472e3f7945
Rafael Winterhalter <rafael.wth@gmail.com>
2016-07-13 01:23:27 +0200

    Added Travis shortcut to not overload integration build.

--- byte-buddy-dep/src/test/java/net/bytebuddy/agent/builder/AgentBuilderDefaultApplicationTest.java
+++ byte-buddy-dep/src/test/java/net/bytebuddy/agent/builder/AgentBuilderDefaultApplicationTest.java
@@ -61,13 +61,17 @@ public class AgentBuilderDefaultApplicationTest {
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
+        // Travis runs out of memory if all of these tests are run. This property serves as a protection.
+        if (Boolean.getBoolean("net.bytebuddy.travis")) {
+            return Arrays.asList(new Object[][]{{AgentBuilder.TypeLocator.Default.FAST}});
+        }
         return Arrays.asList(new Object[][]{
-//                {AgentBuilder.TypeLocator.Default.EXTENDED},
+                {AgentBuilder.TypeLocator.Default.EXTENDED},
                 {AgentBuilder.TypeLocator.Default.FAST},
-//                {AgentBuilder.TypeLocator.Eager.EXTENDED},
-//                {AgentBuilder.TypeLocator.Eager.FAST},
-//                {AgentBuilder.TypeLocator.ClassLoading.EXTENDED},
-//                {AgentBuilder.TypeLocator.ClassLoading.FAST}
+                {AgentBuilder.TypeLocator.Eager.EXTENDED},
+                {AgentBuilder.TypeLocator.Eager.FAST},
+                {AgentBuilder.TypeLocator.ClassLoading.EXTENDED},
+                {AgentBuilder.TypeLocator.ClassLoading.FAST}
         });
     }
 
