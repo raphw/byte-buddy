d659f1e6f0153616eb7acd47722f8085d7edbfbf
Rafael Winterhalter <rafael.wth@gmail.com>
2016-07-19 01:32:03 +0200

    Removed incorrect import.

--- byte-buddy-dep/src/test/java/net/bytebuddy/utility/CompoundListTest.java
+++ byte-buddy-dep/src/test/java/net/bytebuddy/utility/CompoundListTest.java
@@ -5,7 +5,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 import org.mockito.Mock;
-import org.omg.CORBA.Object;
 
 import java.util.Arrays;
 import java.util.List;
