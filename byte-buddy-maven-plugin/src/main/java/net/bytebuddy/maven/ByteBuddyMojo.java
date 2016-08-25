a9bd9334dc0906ef02296d80a4b02099636128cd
Rafael Winterhalter <rafael.wth@gmail.com>
2016-08-25 16:11:50 +0200

    Extended plugin

--- byte-buddy-maven-plugin/src/main/java/net/bytebuddy/maven/ByteBuddyMojo.java
+++ byte-buddy-maven-plugin/src/main/java/net/bytebuddy/maven/ByteBuddyMojo.java
@@ -1,6 +1,7 @@
 package net.bytebuddy.maven;
 
 import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.build.Plugin;
 import net.bytebuddy.description.type.TypeDescription;
 import net.bytebuddy.dynamic.ClassFileLocator;
 import net.bytebuddy.dynamic.DynamicType;
@@ -16,10 +17,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-@Mojo(name = "byte-buddy",
+@Mojo(name = "transform",
         defaultPhase = LifecyclePhase.PROCESS_CLASSES,
         threadSafe = true,
-        requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME)
+        requiresDependencyResolution = ResolutionScope.COMPILE) // TODO: separate mojo
 public class ByteBuddyMojo extends AbstractMojo {
 
     private static final String CLASS_FILE_EXTENSION = ".class";
@@ -30,10 +31,16 @@ public class ByteBuddyMojo extends AbstractMojo {
     @Parameter(name = "rebase", defaultValue = "true", required = true)
     private boolean rebase;
 
+//    @Parameter(name = "includeTest", defaultValue = "false", required = true)
+//    private boolean includeTest;
+
+    @Parameter(name = "plugins", required = true)
+    private List<String> plugins;
+
     @Override
     public void execute() throws MojoExecutionException {
         try {
-            processClasses(new File(mavenProject.getBuild().getDirectory(), "classes"), mavenProject.getCompileClasspathElements());
+            doExecute();
         } catch (IOException exception) {
             throw new MojoExecutionException("Could not process classes", exception);
         } catch (DependencyResolutionRequiredException exception) {
@@ -41,10 +48,29 @@ public class ByteBuddyMojo extends AbstractMojo {
         }
     }
 
-    private void processClasses(File target, List<? extends String> dependencies) throws IOException {
+    private void doExecute() throws DependencyResolutionRequiredException, IOException {
+        processOutputDirectory(mavenProject.getBuild().getOutputDirectory(), mavenProject.getCompileClasspathElements());
+//        if (includeTest) {
+//            processOutputDirectory(mavenProject.getBuild().getTestOutputDirectory(), mavenProject.getTestClasspathElements());
+//        }
+    }
+
+    private void processOutputDirectory(String target, List<? extends String> dependencies) throws IOException {
+        processOutputDirectory(new File(target), dependencies);
+    }
+
+    private void processOutputDirectory(File target, List<? extends String> dependencies) throws IOException {
         getLog().info("Processing classes in: " + target);
         if (!target.isDirectory()) {
             throw new IOException("Source location does not exist: " + target);
+        }
+        List<Plugin> plugins = new ArrayList<Plugin>(this.plugins.size());
+        for (String plugin : this.plugins) {
+            try {
+                plugins.add((Plugin) Class.forName(plugin).getDeclaredConstructor().newInstance());
+            } catch (Exception exception) {
+                throw new RuntimeException("Cannot create plugin: " + plugin, exception);
+            }
         }
         ByteBuddy byteBuddy = new ByteBuddy();
         List<ClassFileLocator> classFileLocators = new ArrayList<ClassFileLocator>(dependencies.size() + 1);
@@ -61,32 +87,46 @@ public class ByteBuddyMojo extends AbstractMojo {
                     classFileLocator,
                     TypePool.Default.ReaderMode.FAST,
                     TypePool.ClassLoading.of(null));
-            processDirectory(target, target, byteBuddy, classFileLocator, typePool);
+            processDirectory(target, target, byteBuddy, classFileLocator, typePool, plugins);
         } finally {
             classFileLocator.close();
         }
     }
 
-    private void processDirectory(File root, File directory, ByteBuddy byteBuddy, ClassFileLocator classFileLocator, TypePool typePool) throws IOException {
+    private void processDirectory(File root, File directory, ByteBuddy byteBuddy, ClassFileLocator classFileLocator, TypePool typePool, List<Plugin> plugins) throws IOException {
         File[] file = directory.listFiles();
         if (file != null) {
             for (File aFile : file) {
-                if (aFile.isFile() && aFile.getName().endsWith(CLASS_FILE_EXTENSION)) {
-                    processClassFile(root, root.toURI().relativize(aFile.toURI()).toString(), byteBuddy, classFileLocator, typePool);
-                } else if (aFile.isDirectory()) {
-                    processDirectory(root, aFile, byteBuddy, classFileLocator, typePool);
+                if (aFile.isDirectory()) {
+                    processDirectory(root, aFile, byteBuddy, classFileLocator, typePool, plugins);
+                } else if (aFile.isFile() && aFile.getName().endsWith(CLASS_FILE_EXTENSION)) {
+                    processClassFile(root, root.toURI().relativize(aFile.toURI()).toString(), byteBuddy, classFileLocator, typePool, plugins);
+                } else {
+                    getLog().debug("Skipping ignored file: " + aFile);
                 }
             }
         }
     }
 
-    private void processClassFile(File root, String classFile, ByteBuddy byteBuddy, ClassFileLocator classFileLocator, TypePool typePool) throws IOException {
+    private void processClassFile(File root, String classFile, ByteBuddy byteBuddy, ClassFileLocator classFileLocator, TypePool typePool, List<Plugin> plugins) throws IOException {
         String typeName = classFile.replace('/', '.').substring(0, classFile.length() - CLASS_FILE_EXTENSION.length());
         getLog().debug("Processing class file: " + typeName);
         TypeDescription typeDescription = typePool.describe(typeName).resolve();
         DynamicType.Builder<?> builder = rebase
                 ? byteBuddy.rebase(typeDescription, classFileLocator)
                 : byteBuddy.redefine(typeDescription, classFileLocator);
-        builder.make().saveIn(root); // TODO: Register processors.
+        boolean transformed = false;
+        for (Plugin plugin : plugins) {
+            if (plugin.matches(typeDescription)) {
+                builder = plugin.apply(builder);
+                transformed = true;
+            }
+        }
+        if (transformed) {
+            getLog().info("Transformed type: " + typeName);
+            builder.make().saveIn(root);
+        } else {
+            getLog().debug("Skipping non-transformed file: " + typeName);
+        }
     }
 }
