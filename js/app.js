88a91b6a58c4afc9ddeb42ac3a81fada1c55978b
Rafael Winterhalter <rafael.wth@gmail.com>
2016-10-17 16:33:27 +0200

    Update to version 1.4.33

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ angular.module('byteBuddy', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.affix', 'd
     .constant('repository', {
         groupId: 'net.bytebuddy',
         artifactId: 'byte-buddy',
-        version: '1.4.32'
+        version: '1.4.33'
     })
 
     .config(function ($routeProvider) {
