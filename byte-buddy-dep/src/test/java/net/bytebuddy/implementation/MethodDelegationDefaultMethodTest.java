4a13381c4a3db69ff6f5ab5ae73b0c87b6624d23
Rafael Winterhalter <rafael.wth@gmail.com>
2016-12-30 14:30:39 +0100

    Added additional tests.

---
+++ byte-buddy-dep/src/test/java/net/bytebuddy/implementation/MethodDelegationDefaultMethodTest.java
@@ -0,0 +1,137 @@
+package net.bytebuddy.implementation;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.modifier.Visibility;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.implementation.bind.annotation.DefaultCall;
+import net.bytebuddy.implementation.bind.annotation.DefaultMethod;
+import net.bytebuddy.implementation.bind.annotation.This;
+import net.bytebuddy.test.utility.JavaVersionRule;
+import org.hamcrest.CoreMatchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.MethodRule;
+
+import java.io.Serializable;
+import java.lang.reflect.Method;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MethodDelegationDefaultMethodTest {
+
+    private static final String FOO = "foo", QUX = "qux";
+
+    private static final String SINGLE_DEFAULT_METHOD = "net.bytebuddy.test.precompiled.SingleDefaultMethodInterface";
+
+    private static final String CONFLICTING_INTERFACE = "net.bytebuddy.test.precompiled.SingleDefaultMethodConflictingInterface";
+
+    private static final String PREFERRING_INTERCEPTOR = "net.bytebuddy.test.precompiled.SingleDefaultMethodPreferringInterceptor";
+
+    private static final String CONFLICTING_PREFERRING_INTERCEPTOR = "net.bytebuddy.test.precompiled.SingleDefaultMethodConflictingPreferringInterceptor";
+
+    @Rule
+    public MethodRule javaVersionRule = new JavaVersionRule();
+
+    @Test
+    @JavaVersionRule.Enforce(8)
+    public void testRunnableDefaultCall() throws Exception {
+        DynamicType.Loaded<?> loaded = new ByteBuddy()
+                .subclass(Object.class)
+                .implement(Class.forName(SINGLE_DEFAULT_METHOD))
+                .intercept(MethodDelegation.to(RunnableClass.class))
+                .make()
+                .load(Class.forName(SINGLE_DEFAULT_METHOD).getClassLoader(), ClassLoadingStrategy.Default.WRAPPER);
+        Object instance = loaded.getLoaded().getDeclaredConstructor().newInstance();
+        Method method = loaded.getLoaded().getMethod(FOO);
+        assertThat(method.invoke(instance), is((Object) QUX));
+    }
+
+    @Test
+    @JavaVersionRule.Enforce(8)
+    public void testCallableDefaultCall() throws Exception {
+        DynamicType.Loaded<?> loaded = new ByteBuddy()
+                .subclass(Object.class)
+                .implement(Class.forName(SINGLE_DEFAULT_METHOD))
+                .intercept(MethodDelegation.to(CallableClass.class))
+                .make()
+                .load(Class.forName(SINGLE_DEFAULT_METHOD).getClassLoader(), ClassLoadingStrategy.Default.WRAPPER);
+        Object instance = loaded.getLoaded().getDeclaredConstructor().newInstance();
+        Method method = loaded.getLoaded().getMethod(FOO);
+        assertThat(method.invoke(instance), is((Object) FOO));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @JavaVersionRule.Enforce(8)
+    public void testImplicitAmbiguousDefaultCallIsBoundToFirst() throws Exception {
+        new ByteBuddy()
+                .subclass(Object.class)
+                .implement(Class.forName(SINGLE_DEFAULT_METHOD), Class.forName(CONFLICTING_INTERFACE))
+                .defineMethod(FOO, Object.class, Visibility.PUBLIC)
+                .intercept(MethodDelegation.to(CallableClass.class))
+                .make();
+    }
+
+    @Test
+    @JavaVersionRule.Enforce(8)
+    public void testExplicitDefaultCall() throws Exception {
+        DynamicType.Loaded<?> loaded = new ByteBuddy()
+                .subclass(Object.class)
+                .implement(Class.forName(SINGLE_DEFAULT_METHOD), Class.forName(CONFLICTING_INTERFACE))
+                .intercept(MethodDelegation.to(Class.forName(PREFERRING_INTERCEPTOR)))
+                .make()
+                .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER);
+        Object instance = loaded.getLoaded().getDeclaredConstructor().newInstance();
+        Method method = loaded.getLoaded().getMethod(FOO);
+        assertThat(method.invoke(instance), is((Object) FOO));
+    }
+
+    @Test
+    @JavaVersionRule.Enforce(8)
+    public void testExplicitDefaultCallToOtherInterface() throws Exception {
+        DynamicType.Loaded<?> loaded = new ByteBuddy()
+                .subclass(Object.class)
+                .implement(Class.forName(SINGLE_DEFAULT_METHOD), Class.forName(CONFLICTING_INTERFACE))
+                .intercept(MethodDelegation.to(Class.forName(CONFLICTING_PREFERRING_INTERCEPTOR)))
+                .make()
+                .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER);
+        Object instance = loaded.getLoaded().getDeclaredConstructor().newInstance();
+        Method method = loaded.getLoaded().getMethod(FOO);
+        assertThat(method.invoke(instance), is((Object) QUX));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    @JavaVersionRule.Enforce(8)
+    public void testIllegalDefaultCallThrowsException() throws Exception {
+        new ByteBuddy()
+                .subclass(Object.class)
+                .implement(Class.forName(SINGLE_DEFAULT_METHOD))
+                .intercept(MethodDelegation.to(IllegalAnnotation.class))
+                .make();
+    }
+
+    public static class RunnableClass {
+
+        public static Object foo(@DefaultCall Runnable runnable) {
+            assertThat(runnable, CoreMatchers.not(instanceOf(Serializable.class)));
+            runnable.run();
+            return QUX;
+        }
+    }
+
+    public static class CallableClass {
+
+        public static String bar(@DefaultMethod Method method, @This Object target) throws Exception {
+            return (String) method.invoke(target);
+        }
+    }
+
+    public static class IllegalAnnotation {
+
+        public static String bar(@DefaultMethod String value) throws Exception {
+            return value;
+        }
+    }
+}
---
+++ byte-buddy-dep/src/test/java/net/bytebuddy/implementation/MethodDelegationSuperMethodTest.java
@@ -0,0 +1,168 @@
+package net.bytebuddy.implementation;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.DynamicType;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import net.bytebuddy.implementation.bind.annotation.SuperMethod;
+import net.bytebuddy.implementation.bind.annotation.This;
+import net.bytebuddy.test.utility.CallTraceable;
+import net.bytebuddy.test.utility.JavaVersionRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.MethodRule;
+
+import java.lang.reflect.Method;
+
+import static net.bytebuddy.matcher.ElementMatchers.isDeclaredBy;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class MethodDelegationSuperMethodTest {
+
+    private static final String SINGLE_DEFAULT_METHOD = "net.bytebuddy.test.precompiled.SingleDefaultMethodInterface";
+
+    private static final String CONFLICTING_INTERFACE = "net.bytebuddy.test.precompiled.SingleDefaultMethodConflictingInterface";
+
+    private static final String FOO = "foo", BAR = "bar";
+
+    @Rule
+    public MethodRule javaVersionRule = new JavaVersionRule();
+
+    @Test
+    public void testRunnableSuperCall() throws Exception {
+        DynamicType.Loaded<Foo> loaded = new ByteBuddy()
+                .subclass(Foo.class)
+                .method(isDeclaredBy(Foo.class))
+                .intercept(MethodDelegation.to(SampleClass.class))
+                .make()
+                .load(Foo.class.getClassLoader(), ClassLoadingStrategy.Default.WRAPPER);
+        Foo instance = loaded.getLoaded().getDeclaredConstructor().newInstance();
+        assertThat(instance.value, is(BAR));
+        instance.foo();
+        assertThat(instance.value, is(FOO));
+    }
+
+    @Test
+    public void testVoidToNonVoidSuperCall() throws Exception {
+        DynamicType.Loaded<VoidTest> loaded = new ByteBuddy()
+                .subclass(VoidTest.class)
+                .method(isDeclaredBy(VoidTest.class))
+                .intercept(MethodDelegation.to(NonVoidTarget.class))
+                .make()
+                .load(VoidTest.class.getClassLoader(), ClassLoadingStrategy.Default.WRAPPER);
+        VoidTest instance = loaded.getLoaded().getDeclaredConstructor().newInstance();
+        instance.foo();
+        instance.assertOnlyCall(FOO);
+    }
+
+    @Test
+    @JavaVersionRule.Enforce(8)
+    public void testDefaultMethodFallback() throws Exception {
+        DynamicType.Loaded<?> loaded = new ByteBuddy()
+                .subclass(Object.class)
+                .implement(Class.forName(SINGLE_DEFAULT_METHOD))
+                .intercept(MethodDelegation.to(NonVoidTarget.class))
+                .make()
+                .load(getClass().getClassLoader(), ClassLoadingStrategy.Default.WRAPPER);
+        Object instance = loaded.getLoaded().getDeclaredConstructor().newInstance();
+        Method method = loaded.getLoaded().getMethod(FOO);
+        assertThat(method.invoke(instance), is((Object) FOO));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @JavaVersionRule.Enforce(8)
+    public void testDefaultMethodFallbackDisabled() throws Exception {
+        new ByteBuddy()
+                .subclass(Object.class)
+                .implement(Class.forName(SINGLE_DEFAULT_METHOD))
+                .intercept(MethodDelegation.to(NoFallback.class))
+                .make();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    @JavaVersionRule.Enforce(8)
+    public void testDefaultMethodFallbackAmbiguous() throws Exception {
+        new ByteBuddy()
+                .subclass(Object.class)
+                .implement(Class.forName(SINGLE_DEFAULT_METHOD), Class.forName(CONFLICTING_INTERFACE))
+                .intercept(MethodDelegation.to(NonVoidTarget.class))
+                .make();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAbstractMethodNonBindable() throws Exception {
+        new ByteBuddy()
+                .subclass(Qux.class)
+                .method(isDeclaredBy(Qux.class))
+                .intercept(MethodDelegation.to(SampleClass.class))
+                .make();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testWrongTypeThrowsException() throws Exception {
+        new ByteBuddy()
+                .subclass(Bar.class)
+                .method(isDeclaredBy(Bar.class))
+                .intercept(MethodDelegation.to(IllegalAnnotation.class))
+                .make();
+    }
+
+    public static class Foo {
+
+        public String value = BAR;
+
+        public void foo() {
+            value = FOO;
+        }
+    }
+
+    public static class SampleClass {
+
+        public static void foo(@SuperMethod Method method, @This Object target) throws Exception {
+            method.invoke(target);
+        }
+    }
+
+    public static class Bar {
+
+        public String bar() {
+            return FOO;
+        }
+    }
+
+    public abstract static class Qux {
+
+        public abstract String bar();
+    }
+
+    public static class VoidTest extends CallTraceable {
+
+        public void foo() {
+            register(FOO);
+        }
+    }
+
+    public static class NonVoidTarget {
+
+        public static Object foo(@SuperMethod Method method, @This Object target) throws Exception {
+            return method.invoke(target);
+        }
+    }
+
+    public static class IllegalAnnotation {
+
+        public static String bar(@SuperMethod String value) throws Exception {
+            return value;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static class NoFallback {
+
+        @RuntimeType
+        public static Object foo(@SuperMethod(fallbackToDefault = false) Method method) throws Exception {
+            return null;
+        }
+    }
+}
