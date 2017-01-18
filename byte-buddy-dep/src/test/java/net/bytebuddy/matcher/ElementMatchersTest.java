605e7d7b886b877b967700d10e95ce7f346e3e55
Rafael Winterhalter <rafael.wth@gmail.com>
2017-01-18 15:48:31 +0100

    Fixed matchers.

--- byte-buddy-dep/src/test/java/net/bytebuddy/matcher/ElementMatchersTest.java
+++ byte-buddy-dep/src/test/java/net/bytebuddy/matcher/ElementMatchersTest.java
@@ -140,7 +140,7 @@ public class ElementMatchersTest {
         assertThat(ElementMatchers.is(field).matches(fieldDescription), is(true));
         assertThat(ElementMatchers.definedField(ElementMatchers.is(fieldDescription.asDefined())).matches(fieldDescription), is(true));
         assertThat(ElementMatchers.is(fieldDescription.asDefined()).matches(fieldDescription.asDefined()), is(true));
-        assertThat(ElementMatchers.is(fieldDescription.asDefined()).matches(fieldDescription), is(false));
+        assertThat(ElementMatchers.is(fieldDescription.asDefined()).matches(fieldDescription), is(true));
         assertThat(ElementMatchers.is(fieldDescription).matches(fieldDescription.asDefined()), is(false));
     }
 
@@ -164,7 +164,7 @@ public class ElementMatchersTest {
         assertThat(ElementMatchers.is(method).matches(methodDescription), is(true));
         assertThat(ElementMatchers.definedMethod(ElementMatchers.is(methodDescription.asDefined())).matches(methodDescription), is(true));
         assertThat(ElementMatchers.is(methodDescription.asDefined()).matches(methodDescription.asDefined()), is(true));
-        assertThat(ElementMatchers.is(methodDescription.asDefined()).matches(methodDescription), is(false));
+        assertThat(ElementMatchers.is(methodDescription.asDefined()).matches(methodDescription), is(true));
         assertThat(ElementMatchers.is(methodDescription).matches(methodDescription.asDefined()), is(false));
     }
 
@@ -176,7 +176,7 @@ public class ElementMatchersTest {
         assertThat(ElementMatchers.is(constructor).matches(methodDescription), is(true));
         assertThat(ElementMatchers.definedMethod(ElementMatchers.is(methodDescription.asDefined())).matches(methodDescription), is(true));
         assertThat(ElementMatchers.is(methodDescription.asDefined()).matches(methodDescription.asDefined()), is(true));
-        assertThat(ElementMatchers.is(methodDescription.asDefined()).matches(methodDescription), is(false));
+        assertThat(ElementMatchers.is(methodDescription.asDefined()).matches(methodDescription), is(true));
         assertThat(ElementMatchers.is(methodDescription).matches(methodDescription.asDefined()), is(false));
     }
 
@@ -186,7 +186,7 @@ public class ElementMatchersTest {
                 .getDeclaredMethods().filter(named(FOO)).getOnly().getParameters().getOnly();
         assertThat(ElementMatchers.definedParameter(ElementMatchers.is(parameterDescription.asDefined())).matches(parameterDescription), is(true));
         assertThat(ElementMatchers.is(parameterDescription.asDefined()).matches(parameterDescription.asDefined()), is(true));
-        assertThat(ElementMatchers.is(parameterDescription.asDefined()).matches(parameterDescription), is(false));
+        assertThat(ElementMatchers.is(parameterDescription.asDefined()).matches(parameterDescription), is(true));
         assertThat(ElementMatchers.is(parameterDescription).matches(parameterDescription.asDefined()), is(false));
     }
 
