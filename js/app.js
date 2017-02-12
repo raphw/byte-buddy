c4cdee4b57324091b2efaaa523d4689158c763b3
Rafael Winterhalter <rafael.wth@gmail.com>
2017-02-12 14:19:40 +0100

    Update to 1.6.8

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ angular.module('byteBuddy', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.affix', 'd
     .constant('repository', {
         groupId: 'net.bytebuddy',
         artifactId: 'byte-buddy',
-        version: '1.6.7'
+        version: '1.6.8'
     })
 
     .config(function ($routeProvider, repository) {
