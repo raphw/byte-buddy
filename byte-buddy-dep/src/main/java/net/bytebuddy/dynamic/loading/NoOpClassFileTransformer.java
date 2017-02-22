72307066f0c239b357597223b5b04d94e968472c
Rafael Winterhalter <rafael.wth@gmail.com>
2017-02-22 23:39:09 +0100

    Added hint for findbugs.

--- byte-buddy-dep/src/main/java/net/bytebuddy/dynamic/loading/NoOpClassFileTransformer.java
+++ byte-buddy-dep/src/main/java/net/bytebuddy/dynamic/loading/NoOpClassFileTransformer.java
@@ -1,5 +1,7 @@
 package net.bytebuddy.dynamic.loading;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
 import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
 
@@ -19,6 +21,7 @@ public enum NoOpClassFileTransformer implements ClassFileTransformer {
     private static final byte[] NO_TRANSFORMATION = null;
 
     @Override
+    @SuppressFBWarnings(value = "EI_EXPOSE_REP", justification = "Array is guaranteed to be null")
     public byte[] transform(ClassLoader loader,
                             String className,
                             Class<?> classBeingRedefined,
