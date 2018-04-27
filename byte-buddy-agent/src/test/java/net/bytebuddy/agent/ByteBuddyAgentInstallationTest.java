f06a8d56aa9bd11e53ece2c3a20c04153007474e
Rafael Winterhalter <rafael.wth@gmail.com>
2018-04-27 19:53:59 +0200

    Add test for agent attachment from a class loader that is not the bootstrap class loader.

--- byte-buddy-agent/src/test/java/net/bytebuddy/agent/ByteBuddyAgentInstallationTest.java
+++ byte-buddy-agent/src/test/java/net/bytebuddy/agent/ByteBuddyAgentInstallationTest.java
@@ -5,7 +5,11 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.MethodRule;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.lang.instrument.Instrumentation;
+import java.net.URL;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -19,5 +23,36 @@ public class ByteBuddyAgentInstallationTest {
     @AgentAttachmentRule.Enforce
     public void testAgentInstallation() throws Exception {
         assertThat(ByteBuddyAgent.install(), instanceOf(Instrumentation.class));
+    }
+
+    @Test
+    @AgentAttachmentRule.Enforce
+    public void testAgentInstallationOtherClassLoader() throws Exception {
+        assertThat(new ClassLoader(null) {
+            @Override
+            protected Class<?> findClass(String name) throws ClassNotFoundException {
+                InputStream in = getResourceAsStream(name.replace('.', '/') + ".class");
+                if (in == null) {
+                    throw new ClassNotFoundException(name);
+                }
+                ByteArrayOutputStream out = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int length;
+                try {
+                    while ((length = in.read(buffer)) != -1) {
+                        out.write(buffer, 0, length);
+                    }
+                } catch (IOException exception) {
+                    throw new AssertionError(exception);
+                }
+                byte[] binaryRepresentation = out.toByteArray();
+                return defineClass(name, binaryRepresentation, 0, binaryRepresentation.length);
+            }
+
+            @Override
+            public InputStream getResourceAsStream(String name) {
+                return ByteBuddyAgentInstallationTest.class.getClassLoader().getResourceAsStream(name);
+            }
+        }.loadClass(ByteBuddyAgent.class.getName()).getDeclaredMethod("install").invoke(null), instanceOf(Instrumentation.class));
     }
 }
