6d7b24f57b927a3bbd7bfa6486db2e6b6ea1a899
Rafael Winterhalter <rafael.wth@gmail.com>
2018-05-25 21:42:04 +0200

    Update to 1.8.12

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ angular.module('byteBuddy', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.affix', 'd
     .constant('repository', {
         groupId: 'net.bytebuddy',
         artifactId: 'byte-buddy',
-        version: '1.8.11'
+        version: '1.8.12'
     })
 
     .config(function ($routeProvider, repository) {
