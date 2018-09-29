1d740cc5ead095f6934dcdf66147067d79656365
Rafael Winterhalter <rafael.wth@gmail.com>
2018-09-29 21:49:55 +0200

    Update to version 1.9.0

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ angular.module('byteBuddy', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.affix', 'd
     .constant('repository', {
         groupId: 'net.bytebuddy',
         artifactId: 'byte-buddy',
-        version: '1.8.22'
+        version: '1.9.0'
     })
 
     .config(function ($routeProvider, repository) {
