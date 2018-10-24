ed571bc2c6f82c9b7aaca1161954471c18f4cf1a
Rafael Winterhalter <rafael.wth@gmail.com>
2018-10-24 21:22:34 +0200

    Add license headers to source files.

--- byte-buddy-dep/src/main/java/net/bytebuddy/dynamic/scaffold/package-info.java
+++ byte-buddy-dep/src/main/java/net/bytebuddy/dynamic/scaffold/package-info.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 - 2018 Rafael Winterhalter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  * This package contains helper types and implementations that are responsible for the actual writing of a byte array
  * representing a Java class. These utilities allow to write a Java type in a more modular manner and take away
--- byte-buddy-dep/src/main/java/net/bytebuddy/implementation/bytecode/package-info.java
+++ byte-buddy-dep/src/main/java/net/bytebuddy/implementation/bytecode/package-info.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 - 2018 Rafael Winterhalter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /**
  * Types and classes in this package are responsible for creating Java byte code for a given byte code target
  * which is represented by a {@link net.bytebuddy.description.method.MethodDescription}.
