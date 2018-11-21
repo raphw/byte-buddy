3af6b65cc774f1d7c6c053ef953afdad4c1eb5c5
Rafael Winterhalter <rafael.wth@gmail.com>
2018-11-21 20:13:43 +0100

    Fix lookup injection into default package

--- byte-buddy-dep/src/test/java/net/bytebuddy/dynamic/loading/ClassInjectorUsingLookupTest.java
+++ byte-buddy-dep/src/test/java/net/bytebuddy/dynamic/loading/ClassInjectorUsingLookupTest.java
@@ -8,15 +8,18 @@ import net.bytebuddy.dynamic.DynamicType;
 import net.bytebuddy.implementation.MethodCall;
 import net.bytebuddy.test.utility.AgentAttachmentRule;
 import net.bytebuddy.test.utility.JavaVersionRule;
+import net.bytebuddy.utility.JavaType;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.MethodRule;
 
+import java.lang.reflect.Constructor;
 import java.util.Collections;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class ClassInjectorUsingLookupTest {
 
@@ -71,6 +74,35 @@ public class ClassInjectorUsingLookupTest {
                 .make();
         assertThat(injector.inject(Collections.singletonMap(dynamicType.getTypeDescription(), dynamicType.getBytes()))
                 .get(dynamicType.getTypeDescription()).getName(), is("net.bytebuddy.test.Bar"));
+    }
+
+
+    @Test
+    @JavaVersionRule.Enforce(9)
+    public void testSubclassUsingLookup() throws Exception {
+        Class<?> defaultPackageType = new ByteBuddy()
+                .subclass(Object.class)
+                .name("DefaultPackageType")
+                .make()
+                .load(ClassLoadingStrategy.BOOTSTRAP_LOADER, ClassLoadingStrategy.Default.WRAPPER)
+                .getLoaded();
+        Object privateLookup = JavaType.METHOD_HANDLES.load()
+                .getMethod("privateLookupIn", Class.class, JavaType.METHOD_HANDLES_LOOKUP.load())
+                .invoke(null, defaultPackageType, JavaType.METHOD_HANDLES.load().getMethod("lookup").invoke(null));
+        Class<?> type = new ByteBuddy()
+                .subclass(defaultPackageType)
+                .make()
+                .load(defaultPackageType.getClassLoader(), ClassLoadingStrategy.UsingLookup.of(privateLookup))
+                .getLoaded();
+        assertThat(type.getDeclaredMethods().length, is(0));
+        assertThat(type.getDeclaredConstructors().length, is(1));
+        assertThat(type.getDeclaredConstructor(), notNullValue(Constructor.class));
+        assertThat(defaultPackageType.isAssignableFrom(type), is(true));
+        assertThat(type, not(is((Object) defaultPackageType)));
+        assertThat(type.getDeclaredConstructor().newInstance(), instanceOf(defaultPackageType));
+        assertTrue(defaultPackageType.isInstance(type.getDeclaredConstructor().newInstance()));
+        assertThat(type.isInterface(), is(false));
+        assertThat(type.isAnnotation(), is(false));
     }
 
     @Test
