2d616c6ccbf195e7b217279a424c55e559e7ae92
Rafael Winterhalter <rafael.wth@gmail.com>
2019-07-12 00:59:02 +0200

    Restrict test to HotSpot.

--- byte-buddy-dep/src/test/java/net/bytebuddy/dynamic/loading/ClassReloadingStrategyTest.java
+++ byte-buddy-dep/src/test/java/net/bytebuddy/dynamic/loading/ClassReloadingStrategyTest.java
@@ -200,7 +200,7 @@ public class ClassReloadingStrategyTest {
     }
 
     @Test
-    @JavaVersionRule.Enforce(value = 8, atMost = 8)
+    @JavaVersionRule.Enforce(hotSpot = 8)
     @AgentAttachmentRule.Enforce(retransformsClasses = true)
     public void testAnonymousType() throws Exception {
         ClassLoader classLoader = new ByteArrayClassLoader(ClassLoadingStrategy.BOOTSTRAP_LOADER,
