80a3233a4aa3aea5adc21ec0c44ccd511fbec936
Rafael Winterhalter <rafael.wth@gmail.com>
2019-07-14 01:27:40 +0200

    Fix HotSpot attachment tests.

--- byte-buddy-agent/src/test/java/net/bytebuddy/agent/VirtualMachineForHotSpotTest.java
+++ byte-buddy-agent/src/test/java/net/bytebuddy/agent/VirtualMachineForHotSpotTest.java
@@ -1,26 +1,27 @@
 package net.bytebuddy.agent;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
 public class VirtualMachineForHotSpotTest {
 
-    // TODO: Adjust tests after API correction
-
-    /*@Test
+    @Test
     public void testAttachment() throws Exception {
         VirtualMachine.ForHotSpot.Connection connection = mock(VirtualMachine.ForHotSpot.Connection.class);
-        when(connection.read(any(byte[].class))).then(new ByteAnswer("0".getBytes("UTF-8"))).then(new ByteAnswer((byte) 10));
+        VirtualMachine.ForHotSpot.Connection.Response response = mock(VirtualMachine.ForHotSpot.Connection.Response.class);
+        when(connection.execute("1", "load", "instrument", "false", "foo=bar")).thenReturn(response);
+        when(response.read(any(byte[].class))).then(new ByteAnswer("0".getBytes("UTF-8"))).then(new ByteAnswer((byte) 10));
         VirtualMachine virtualMachine = new VirtualMachine.ForHotSpot(connection);
         virtualMachine.loadAgent("foo", "bar");
-        InOrder order = inOrder(connection);
-        order.verify(connection).write("1".getBytes("UTF-8"));
-        order.verify(connection).write(new byte[1]);
-        order.verify(connection).write("load".getBytes("UTF-8"));
-        order.verify(connection).write(new byte[1]);
-        order.verify(connection).write("instrument".getBytes("UTF-8"));
-        order.verify(connection).write(new byte[1]);
-        order.verify(connection).write(Boolean.FALSE.toString().getBytes("UTF-8"));
-        order.verify(connection).write(new byte[1]);
-        order.verify(connection).write("foo=bar".getBytes("UTF-8"));
-        order.verify(connection).write(new byte[1]);
+        verify(connection).execute("1", "load", "instrument", "false", "foo=bar");
+        verify(response).release();
+        verifyNoMoreInteractions(connection);
         virtualMachine.detach();
         verify(connection).close();
     }
@@ -28,29 +29,39 @@ public class VirtualMachineForHotSpotTest {
     @Test
     public void testAttachmentWithoutArgument() throws Exception {
         VirtualMachine.ForHotSpot.Connection connection = mock(VirtualMachine.ForHotSpot.Connection.class);
-        when(connection.read(any(byte[].class))).then(new ByteAnswer("0".getBytes("UTF-8"))).then(new ByteAnswer((byte) 10));
+        VirtualMachine.ForHotSpot.Connection.Response response = mock(VirtualMachine.ForHotSpot.Connection.Response.class);
+        when(connection.execute("1", "load", "instrument", "false", "foo")).thenReturn(response);
+        when(response.read(any(byte[].class))).then(new ByteAnswer("0".getBytes("UTF-8"))).then(new ByteAnswer((byte) 10));
         VirtualMachine virtualMachine = new VirtualMachine.ForHotSpot(connection);
         virtualMachine.loadAgent("foo", null);
-        InOrder order = inOrder(connection);
-        order.verify(connection).write("1".getBytes("UTF-8"));
-        order.verify(connection).write(new byte[1]);
-        order.verify(connection).write("load".getBytes("UTF-8"));
-        order.verify(connection).write(new byte[1]);
-        order.verify(connection).write("instrument".getBytes("UTF-8"));
-        order.verify(connection).write(new byte[1]);
-        order.verify(connection).write(Boolean.FALSE.toString().getBytes("UTF-8"));
-        order.verify(connection).write(new byte[1]);
-        order.verify(connection).write("foo".getBytes("UTF-8"));
-        order.verify(connection).write(new byte[1]);
+        verify(connection).execute("1", "load", "instrument", "false", "foo");
+        verify(response).release();
+        verifyNoMoreInteractions(connection);
         virtualMachine.detach();
         verify(connection).close();
     }
 
+    @Test
+    public void testNativeAttachment() throws Exception {
+        VirtualMachine.ForHotSpot.Connection connection = mock(VirtualMachine.ForHotSpot.Connection.class);
+        VirtualMachine.ForHotSpot.Connection.Response response = mock(VirtualMachine.ForHotSpot.Connection.Response.class);
+        when(connection.execute("1", "load", "instrument", "true", "foo=bar")).thenReturn(response);
+        when(response.read(any(byte[].class))).then(new ByteAnswer("0".getBytes("UTF-8"))).then(new ByteAnswer((byte) 10));
+        VirtualMachine virtualMachine = new VirtualMachine.ForHotSpot(connection);
+        virtualMachine.loadAgentPath("foo", "bar");
+        verify(connection).execute("1", "load", "instrument", "true", "foo=bar");
+        verify(response).release();
+        verifyNoMoreInteractions(connection);
+        virtualMachine.detach();
+        verify(connection).close();
+    }
 
     @Test(expected = IOException.class)
     public void testAttachmentIncompatibleProtocol() throws Exception {
         VirtualMachine.ForHotSpot.Connection connection = mock(VirtualMachine.ForHotSpot.Connection.class);
-        when(connection.read(any(byte[].class)))
+        VirtualMachine.ForHotSpot.Connection.Response response = mock(VirtualMachine.ForHotSpot.Connection.Response.class);
+        when(connection.execute(Mockito.<String[]>any())).thenReturn(response);
+        when(response.read(any(byte[].class)))
                 .then(new ByteAnswer("1".getBytes("UTF-8")))
                 .then(new ByteAnswer("0".getBytes("UTF-8")))
                 .then(new ByteAnswer("1".getBytes("UTF-8")))
@@ -61,7 +72,9 @@ public class VirtualMachineForHotSpotTest {
     @Test(expected = IllegalStateException.class)
     public void testAttachmentUnknownError() throws Exception {
         VirtualMachine.ForHotSpot.Connection connection = mock(VirtualMachine.ForHotSpot.Connection.class);
-        when(connection.read(any(byte[].class)))
+        VirtualMachine.ForHotSpot.Connection.Response response = mock(VirtualMachine.ForHotSpot.Connection.Response.class);
+        when(connection.execute(Mockito.<String[]>any())).thenReturn(response);
+        when(response.read(any(byte[].class)))
                 .then(new ByteAnswer("1".getBytes("UTF-8")))
                 .then(new ByteAnswer((byte) 10))
                 .then(new ByteAnswer("foo".getBytes("UTF-8")))
@@ -81,5 +94,5 @@ public class VirtualMachineForHotSpotTest {
             System.arraycopy(value, 0, invocationOnMock.getArgument(0), 0, value.length);
             return value.length;
         }
-    }*/
+    }
 }
