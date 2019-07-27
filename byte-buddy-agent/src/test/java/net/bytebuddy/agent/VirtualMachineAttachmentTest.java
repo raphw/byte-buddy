8259a6d40a0394b09a2dc32e81bbb27f2a508cc8
Rafael Winterhalter <rafael.wth@gmail.com>
2019-07-27 12:32:34 +0200

    Add timeout for attachment test.

--- byte-buddy-agent/src/test/java/net/bytebuddy/agent/VirtualMachineAttachmentTest.java
+++ byte-buddy-agent/src/test/java/net/bytebuddy/agent/VirtualMachineAttachmentTest.java
@@ -43,7 +43,7 @@ public class VirtualMachineAttachmentTest {
         SampleAgent.argument = null;
     }
 
-    @Test
+    @Test(timeout = 5000L)
     public void testAttachment() throws Exception {
         assertThat(SampleAgent.argument, nullValue(String.class));
         VirtualMachine virtualMachine = (VirtualMachine) VirtualMachine.Resolver.INSTANCE.run()
