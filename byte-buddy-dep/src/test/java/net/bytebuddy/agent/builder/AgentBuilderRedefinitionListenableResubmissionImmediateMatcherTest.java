4e32cc4c1a6db36f033d2465363430ac340532d5
Rafael Winterhalter <rafael.wth@gmail.com>
2021-03-21 22:48:11 +0100

    Add tests for resubmission listeners.

---
+++ byte-buddy-dep/src/test/java/net/bytebuddy/agent/builder/AgentBuilderRedefinitionListenableResubmissionImmediateMatcherTest.java
@@ -0,0 +1,85 @@
+package net.bytebuddy.agent.builder;
+
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.test.utility.MockitoRule;
+import net.bytebuddy.utility.JavaModule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.mockito.Mock;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class AgentBuilderRedefinitionListenableResubmissionImmediateMatcherTest {
+
+    private static final String FOO = "foo";
+
+    @Rule
+    public TestRule mockitoRule = new MockitoRule(this);
+
+    @Mock
+    private ClassLoader classLoader;
+
+    @Mock
+    private JavaModule module;
+
+    @Mock
+    private AgentBuilder.RedefinitionListenable.ResubmissionImmediateMatcher left;
+
+    @Mock
+    private AgentBuilder.RedefinitionListenable.ResubmissionImmediateMatcher right;
+
+    @Test
+    public void testTrivialMatching() {
+        assertThat(AgentBuilder.RedefinitionListenable.ResubmissionImmediateMatcher.Trivial.MATCHING.matches(FOO,
+                classLoader,
+                module), is(true));
+    }
+
+    @Test
+    public void testTrivialNonMatching() {
+        assertThat(AgentBuilder.RedefinitionListenable.ResubmissionImmediateMatcher.Trivial.NON_MATCHING.matches(FOO,
+                classLoader,
+                module), is(false));
+    }
+
+    @Test
+    public void testForElementMatchers() {
+        assertThat(new AgentBuilder.RedefinitionListenable.ResubmissionImmediateMatcher.ForElementMatchers(ElementMatchers.<String>is(FOO),
+                ElementMatchers.is(classLoader),
+                ElementMatchers.is(module)).matches(FOO,
+                classLoader,
+                module), is(true));
+    }
+
+    @Test
+    public void testConjunction() {
+        when(left.matches(FOO, classLoader, module)).thenReturn(true);
+        when(right.matches(FOO, classLoader, module)).thenReturn(true);
+
+        assertThat(new AgentBuilder.RedefinitionListenable.ResubmissionImmediateMatcher.Conjunction(left, right).matches(FOO,
+                classLoader,
+                module), is(true));
+
+        verify(left).matches(FOO, classLoader, module);
+        verifyNoMoreInteractions(left);
+        verify(right).matches(FOO, classLoader, module);
+        verifyNoMoreInteractions(right);
+    }
+
+    @Test
+    public void testDisjunction() {
+        when(left.matches(FOO, classLoader, module)).thenReturn(true);
+        when(right.matches(FOO, classLoader, module)).thenReturn(true);
+
+        assertThat(new AgentBuilder.RedefinitionListenable.ResubmissionImmediateMatcher.Disjunction(left, right).matches(FOO,
+                classLoader,
+                module), is(true));
+
+        verify(left).matches(FOO, classLoader, module);
+        verifyNoMoreInteractions(left);
+        verifyZeroInteractions(right);
+    }
+}
---
+++ byte-buddy-dep/src/test/java/net/bytebuddy/agent/builder/AgentBuilderRedefinitionListenableResubmissionOnErrorMatcherTest.java
@@ -0,0 +1,94 @@
+package net.bytebuddy.agent.builder;
+
+import net.bytebuddy.matcher.ElementMatchers;
+import net.bytebuddy.test.utility.MockitoRule;
+import net.bytebuddy.utility.JavaModule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestRule;
+import org.mockito.Mock;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+
+public class AgentBuilderRedefinitionListenableResubmissionOnErrorMatcherTest {
+
+    private static final String FOO = "foo";
+
+    @Rule
+    public TestRule mockitoRule = new MockitoRule(this);
+
+    @Mock
+    private Throwable throwable;
+
+    @Mock
+    private ClassLoader classLoader;
+
+    @Mock
+    private JavaModule module;
+
+    @Mock
+    private AgentBuilder.RedefinitionListenable.ResubmissionOnErrorMatcher left;
+
+    @Mock
+    private AgentBuilder.RedefinitionListenable.ResubmissionOnErrorMatcher right;
+
+    @Test
+    public void testTrivialMatching() {
+        assertThat(AgentBuilder.RedefinitionListenable.ResubmissionOnErrorMatcher.Trivial.MATCHING.matches(throwable,
+                FOO,
+                classLoader,
+                module), is(true));
+    }
+
+    @Test
+    public void testTrivialNonMatching() {
+        assertThat(AgentBuilder.RedefinitionListenable.ResubmissionOnErrorMatcher.Trivial.NON_MATCHING.matches(throwable,
+                FOO,
+                classLoader,
+                module), is(false));
+    }
+
+    @Test
+    public void testForElementMatchers() {
+        assertThat(new AgentBuilder.RedefinitionListenable.ResubmissionOnErrorMatcher.ForElementMatchers(ElementMatchers.is(throwable),
+                ElementMatchers.<String>is(FOO),
+                ElementMatchers.is(classLoader),
+                ElementMatchers.is(module)).matches(throwable,
+                FOO,
+                classLoader,
+                module), is(true));
+    }
+
+    @Test
+    public void testConjunction() {
+        when(left.matches(throwable, FOO, classLoader, module)).thenReturn(true);
+        when(right.matches(throwable, FOO, classLoader, module)).thenReturn(true);
+
+        assertThat(new AgentBuilder.RedefinitionListenable.ResubmissionOnErrorMatcher.Conjunction(left, right).matches(throwable,
+                FOO,
+                classLoader,
+                module), is(true));
+
+        verify(left).matches(throwable, FOO, classLoader, module);
+        verifyNoMoreInteractions(left);
+        verify(right).matches(throwable, FOO, classLoader, module);
+        verifyNoMoreInteractions(right);
+    }
+
+    @Test
+    public void testDisjunction() {
+        when(left.matches(throwable, FOO, classLoader, module)).thenReturn(true);
+        when(right.matches(throwable, FOO, classLoader, module)).thenReturn(true);
+
+        assertThat(new AgentBuilder.RedefinitionListenable.ResubmissionOnErrorMatcher.Disjunction(left, right).matches(throwable,
+                FOO,
+                classLoader,
+                module), is(true));
+
+        verify(left).matches(throwable, FOO, classLoader, module);
+        verifyNoMoreInteractions(left);
+        verifyZeroInteractions(right);
+    }
+}
