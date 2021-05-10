dabd0df89ad15a958164a915e55a8581602c41c2
Firestar99 <4696087-Firestar99@users.noreply.gitlab.com>
2021-05-10 16:22:00 +0200

    added tests for new primitive operations

--- byte-buddy-dep/src/test/java/net/bytebuddy/implementation/bytecode/PrimitiveOperationTest.java
+++ byte-buddy-dep/src/test/java/net/bytebuddy/implementation/bytecode/PrimitiveOperationTest.java
@@ -19,7 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.*;
 
 @RunWith(Parameterized.class)
-public class AdditionTest {
+public class PrimitiveOperationTest {
 
     @Parameterized.Parameters
     public static Collection<Object[]> data() {
@@ -28,6 +28,33 @@ public class AdditionTest {
                 {Addition.LONG, StackSize.DOUBLE, Opcodes.LADD},
                 {Addition.FLOAT, StackSize.SINGLE, Opcodes.FADD},
                 {Addition.DOUBLE, StackSize.DOUBLE, Opcodes.DADD},
+        
+                {Substraction.INTEGER, StackSize.SINGLE, Opcodes.ISUB},
+                {Substraction.LONG, StackSize.DOUBLE, Opcodes.LSUB},
+                {Substraction.FLOAT, StackSize.SINGLE, Opcodes.FSUB},
+                {Substraction.DOUBLE, StackSize.DOUBLE, Opcodes.DSUB},
+        
+                {Multiplication.INTEGER, StackSize.SINGLE, Opcodes.IMUL},
+                {Multiplication.LONG, StackSize.DOUBLE, Opcodes.LMUL},
+                {Multiplication.FLOAT, StackSize.SINGLE, Opcodes.FMUL},
+                {Multiplication.DOUBLE, StackSize.DOUBLE, Opcodes.DMUL},
+        
+                {Division.INTEGER, StackSize.SINGLE, Opcodes.IDIV},
+                {Division.LONG, StackSize.DOUBLE, Opcodes.LDIV},
+                {Division.FLOAT, StackSize.SINGLE, Opcodes.FDIV},
+                {Division.DOUBLE, StackSize.DOUBLE, Opcodes.DDIV},
+        
+                {Remainder.INTEGER, StackSize.SINGLE, Opcodes.IREM},
+                {Remainder.LONG, StackSize.DOUBLE, Opcodes.LREM},
+                {Remainder.FLOAT, StackSize.SINGLE, Opcodes.FREM},
+                {Remainder.DOUBLE, StackSize.DOUBLE, Opcodes.DREM},
+        
+                {ShiftLeft.INTEGER, StackSize.SINGLE, Opcodes.ISHL},
+                {ShiftLeft.LONG, StackSize.DOUBLE, Opcodes.LSHL},
+                {ShiftRight.INTEGER, StackSize.SINGLE, Opcodes.ISHR},
+                {ShiftRight.LONG, StackSize.DOUBLE, Opcodes.LSHR},
+                {ShiftRightUnsigned.INTEGER, StackSize.SINGLE, Opcodes.IUSHR},
+                {ShiftRightUnsigned.LONG, StackSize.DOUBLE, Opcodes.LUSHR},
         });
     }
 
@@ -37,7 +64,7 @@ public class AdditionTest {
 
     private final int opcodes;
 
-    public AdditionTest(StackManipulation stackManipulation, StackSize stackSize, int opcodes) {
+    public PrimitiveOperationTest(StackManipulation stackManipulation, StackSize stackSize, int opcodes) {
         this.stackManipulation = stackManipulation;
         this.stackSize = stackSize;
         this.opcodes = opcodes;
@@ -53,7 +80,7 @@ public class AdditionTest {
     private Implementation.Context implementationContext;
 
     @Test
-    public void testAddition() throws Exception {
+    public void testAddition() {
         StackManipulation.Size size = stackManipulation.apply(methodVisitor, implementationContext);
         assertThat(size.getMaximalSize(), is(0));
         assertThat(size.getSizeImpact(), is(-stackSize.getSize()));
