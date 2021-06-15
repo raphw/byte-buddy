0a778db82fb35b0165d5ec6e1cd404a4fa33b38f
Rafael Winterhalter <rafael.wth@gmail.com>
2021-06-15 11:06:08 +0200

    Use task execution graph.

--- byte-buddy-gradle-plugin/src/main/java/net/bytebuddy/build/gradle/AbstractByteBuddyTaskConfiguration.java
+++ byte-buddy-gradle-plugin/src/main/java/net/bytebuddy/build/gradle/AbstractByteBuddyTaskConfiguration.java
@@ -18,6 +18,7 @@ package net.bytebuddy.build.gradle;
 import org.gradle.api.Action;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
+import org.gradle.api.execution.TaskExecutionGraph;
 import org.gradle.api.file.SourceDirectorySet;
 import org.gradle.api.tasks.SourceSet;
 import org.gradle.api.tasks.compile.JavaCompile;
@@ -64,21 +65,93 @@ public abstract class AbstractByteBuddyTaskConfiguration<
     /**
      * {@inheritDoc}
      */
-    public void execute(Project project) {
-        @SuppressWarnings("unchecked")
-        S extension = (S) project.getExtensions().getByName(name);
+    public void execute(final Project project) {
+        @SuppressWarnings("unchecked") final S extension = (S) project.getExtensions().getByName(name);
         if (extension.getTransformations().isEmpty()) {
             project.getLogger().debug("Not configuring task for source set '{}' as no transformations are defined", sourceSet.getName());
         } else {
             project.getLogger().debug("Configuring Byte Buddy task for source set '{}' as '{}'", sourceSet.getName(), name);
-            JavaCompile compileTask = (JavaCompile) project.getTasks().getByName(sourceSet.getCompileJavaTaskName());
-            T byteBuddyTask = project.getTasks().create(name, extension.toType());
+            final JavaCompile compileTask = (JavaCompile) project.getTasks().getByName(sourceSet.getCompileJavaTaskName());
+            final T byteBuddyTask = project.getTasks().create(name, extension.toType());
             byteBuddyTask.setGroup("Byte Buddy");
             byteBuddyTask.setDescription("Transforms the classes compiled by " + compileTask.getName());
             byteBuddyTask.dependsOn(compileTask);
             extension.configure(byteBuddyTask);
             configureDirectories(sourceSet.getJava(), compileTask, byteBuddyTask);
-            for (Map.Entry<Project, Set<Task>> entry : extension.getAdjustment().resolve(project).entrySet()) {
+            project.getGradle().getTaskGraph().whenReady(new TaskExecutionGraphAdjustmentAction(project,
+                    name,
+                    extension.getAdjustment(),
+                    byteBuddyTask,
+                    compileTask));
+        }
+    }
+
+    /**
+     * Configures the directories of the compile and Byte Buddy tasks.
+     *
+     * @param source        The source directory set.
+     * @param compileTask   The compile task.
+     * @param byteBuddyTask The Byte Buddy task.
+     */
+    protected abstract void configureDirectories(SourceDirectorySet source, JavaCompile compileTask, T byteBuddyTask);
+
+    /**
+     * An action to adjust the task execution graph to depend on the injected Byte Buddy task if a task
+     * depends on the compile task that is being enhanced.
+     */
+    protected static class TaskExecutionGraphAdjustmentAction implements Action<TaskExecutionGraph> {
+
+        /**
+         * The current project.
+         */
+        private final Project project;
+
+        /**
+         * The name of the task.
+         */
+        private final String name;
+
+        /**
+         * The adjustment to apply.
+         */
+        private final Adjustment adjustment;
+
+        /**
+         * The Byte Buddy task that is injected.
+         */
+        private final Task byteBuddyTask;
+
+        /**
+         * The compile task to which the Byte Buddy task is appended to.
+         */
+        private final Task compileTask;
+
+        /**
+         * Creates a new task execution graph adjustment action.
+         *
+         * @param project       The current project.
+         * @param name          The name of the task.
+         * @param adjustment    The adjustment to apply.
+         * @param byteBuddyTask The Byte Buddy task that is injected.
+         * @param compileTask   The compile task to which the Byte Buddy task is appended to.
+         */
+        protected TaskExecutionGraphAdjustmentAction(Project project,
+                                                     String name,
+                                                     Adjustment adjustment,
+                                                     Task byteBuddyTask,
+                                                     Task compileTask) {
+            this.project = project;
+            this.name = name;
+            this.adjustment = adjustment;
+            this.byteBuddyTask = byteBuddyTask;
+            this.compileTask = compileTask;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        public void execute(TaskExecutionGraph graph) {
+            for (Map.Entry<Project, Set<Task>> entry : adjustment.resolve(project).entrySet()) {
                 for (Task task : entry.getValue()) {
                     if (!(task.getName().equals(name)
                             && task.getProject().equals(project))
@@ -94,13 +167,4 @@ public abstract class AbstractByteBuddyTaskConfiguration<
             }
         }
     }
-
-    /**
-     * Configures the directories of the compile and Byte Buddy tasks.
-     *
-     * @param source        The source directory set.
-     * @param compileTask   The compile task.
-     * @param byteBuddyTask The Byte Buddy task.
-     */
-    protected abstract void configureDirectories(SourceDirectorySet source, JavaCompile compileTask, T byteBuddyTask);
 }
