1a631649e96f1c7c0521c2f47a299e2789dc94ba
Rafael Winterhalter <rafael.wth@gmail.com>
2021-10-08 13:11:52 +0200

    Remove field from null constant.

--- byte-buddy-dep/src/main/java/net/bytebuddy/implementation/bytecode/constant/NullConstant.java
+++ byte-buddy-dep/src/main/java/net/bytebuddy/implementation/bytecode/constant/NullConstant.java
@@ -17,7 +17,6 @@ package net.bytebuddy.implementation.bytecode.constant;
 
 import net.bytebuddy.implementation.Implementation;
 import net.bytebuddy.implementation.bytecode.StackManipulation;
-import net.bytebuddy.implementation.bytecode.StackSize;
 import org.objectweb.asm.MethodVisitor;
 import org.objectweb.asm.Opcodes;
 
@@ -29,21 +28,7 @@ public enum NullConstant implements StackManipulation {
     /**
      * The singleton instance.
      */
-    INSTANCE(StackSize.SINGLE);
-
-    /**
-     * The size impact of loading the {@code null} reference onto the operand stack.
-     */
-    private final Size size;
-
-    /**
-     * Creates a null constant.
-     *
-     * @param size The size of the constant on the operand stack.
-     */
-    NullConstant(StackSize size) {
-        this.size = size.toIncreasingSize();
-    }
+    INSTANCE;
 
     /**
      * {@inheritDoc}
@@ -57,6 +42,6 @@ public enum NullConstant implements StackManipulation {
      */
     public Size apply(MethodVisitor methodVisitor, Implementation.Context implementationContext) {
         methodVisitor.visitInsn(Opcodes.ACONST_NULL);
-        return size;
+        return new Size(1, 1);
     }
 }
