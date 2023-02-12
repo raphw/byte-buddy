7d9237bf8d4680ab47c778ac0c2580770619fa9b
Rafael Winterhalter <rafael.wth@gmail.com>
2023-02-12 22:35:37 +0100

    Add additional tests.

---
+++ byte-buddy-dep/src/test/java/net/bytebuddy/implementation/bind/annotation/FieldGetterHandleBinderTest.java
@@ -0,0 +1,114 @@
+package net.bytebuddy.implementation.bind.annotation;
+
+import net.bytebuddy.description.annotation.AnnotationList;
+import net.bytebuddy.description.field.FieldDescription;
+import net.bytebuddy.description.field.FieldList;
+import net.bytebuddy.description.method.ParameterDescription;
+import net.bytebuddy.description.method.ParameterList;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.implementation.bind.MethodDelegationBinder;
+import net.bytebuddy.implementation.bytecode.assign.Assigner;
+import net.bytebuddy.utility.JavaType;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+public class FieldGetterHandleBinderTest extends AbstractAnnotationBinderTest<FieldGetterHandle> {
+
+    private static final String FOO = "foo", BAR = "bar";
+
+    @Mock
+    private FieldDescription.InDefinedShape fieldDescription;
+
+    @Mock
+    private TypeDescription.Generic fieldType, targetType;
+
+    @Mock
+    private TypeDescription rawFieldType, rawTargetType, rawDeclaringType;
+
+    public FieldGetterHandleBinderTest() {
+        super(FieldGetterHandle.class);
+    }
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        when(fieldDescription.asDefined()).thenReturn(fieldDescription);
+        when(fieldDescription.getType()).thenReturn(fieldType);
+        when(fieldDescription.getDeclaringType()).thenReturn(rawDeclaringType);
+        when(target.getType()).thenReturn(targetType);
+        when(fieldType.asErasure()).thenReturn(rawFieldType);
+        when(targetType.asErasure()).thenReturn(rawTargetType);
+    }
+
+    protected TargetMethodAnnotationDrivenBinder.ParameterBinder<FieldGetterHandle> getSimpleBinder() {
+        return FieldGetterHandle.Binder.INSTANCE;
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testIllegalParameter() throws Exception {
+        doReturn(void.class).when(annotation).declaringType();
+        when(annotation.value()).thenReturn(FOO);
+        when(instrumentedType.getDeclaredFields()).thenReturn(new FieldList.Explicit<FieldDescription.InDefinedShape>(fieldDescription));
+        when(fieldDescription.getActualName()).thenReturn(FOO);
+        when(fieldDescription.isVisibleTo(instrumentedType)).thenReturn(true);
+        when(target.getDeclaredAnnotations()).thenReturn(new AnnotationList.Empty());
+        when(stackManipulation.isValid()).thenReturn(true);
+        FieldGetterHandle.Binder.INSTANCE.bind(annotationDescription,
+                source,
+                target,
+                implementationTarget,
+                assigner,
+                Assigner.Typing.STATIC);
+    }
+
+    @Test
+    public void testLegalAssignment() throws Exception {
+        when(rawTargetType.isAssignableFrom(JavaType.METHOD_HANDLE.getTypeStub())).thenReturn(true);
+        doReturn(void.class).when(annotation).declaringType();
+        when(annotation.value()).thenReturn(FOO);
+        when(instrumentedType.getDeclaredFields()).thenReturn(new FieldList.Explicit<FieldDescription.InDefinedShape>(fieldDescription));
+        when(fieldDescription.getActualName()).thenReturn(FOO);
+        when(fieldDescription.isVisibleTo(instrumentedType)).thenReturn(true);
+        when(target.getDeclaredAnnotations()).thenReturn(new AnnotationList.Empty());
+        when(stackManipulation.isValid()).thenReturn(true);
+        MethodDelegationBinder.ParameterBinding<?> binding = FieldGetterHandle.Binder.INSTANCE.bind(annotationDescription,
+                source,
+                target,
+                implementationTarget,
+                assigner,
+                Assigner.Typing.STATIC);
+        assertThat(binding.isValid(), is(true));
+    }
+
+    @Test
+    public void testLegalStaticAssignment() throws Exception {
+        when(rawTargetType.isAssignableFrom(JavaType.METHOD_HANDLE.getTypeStub())).thenReturn(true);
+        doReturn(void.class).when(annotation).declaringType();
+        when(annotation.value()).thenReturn(FOO);
+        when(instrumentedType.getDeclaredFields()).thenReturn(new FieldList.Explicit<FieldDescription.InDefinedShape>(fieldDescription));
+        when(fieldDescription.isStatic()).thenReturn(true);
+        when(fieldDescription.getActualName()).thenReturn(FOO);
+        when(fieldDescription.isVisibleTo(instrumentedType)).thenReturn(true);
+        when(target.getDeclaredAnnotations()).thenReturn(new AnnotationList.Empty());
+        when(stackManipulation.isValid()).thenReturn(true);
+        MethodDelegationBinder.ParameterBinding<?> binding = FieldGetterHandle.Binder.INSTANCE.bind(annotationDescription,
+                source,
+                target,
+                implementationTarget,
+                assigner,
+                Assigner.Typing.STATIC);
+        assertThat(binding.isValid(), is(true));
+    }
+
+    public static class Foo {
+
+        public String foo;
+    }
+}
