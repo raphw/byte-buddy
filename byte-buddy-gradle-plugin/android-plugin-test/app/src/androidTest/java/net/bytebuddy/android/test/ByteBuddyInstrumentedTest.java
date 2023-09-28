e7a3c0da783465f7fb27208c5364e0324e341ec4
Cesar Munoz <likethesalad@gmail.com>
2023-09-28 15:40:02 +0200

    Clean up tests

--- byte-buddy-gradle-plugin/android-plugin-test/app/src/androidTest/java/net/bytebuddy/android/test/ByteBuddyInstrumentedTest.java
+++ byte-buddy-gradle-plugin/android-plugin-test/app/src/androidTest/java/net/bytebuddy/android/test/ByteBuddyInstrumentedTest.java
@@ -1,40 +1,28 @@
 package net.bytebuddy.android.test;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import androidx.test.ext.junit.rules.ActivityScenarioRule;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
 import com.service.definition.ServiceDefinition;
 
-import org.junit.Rule;
+import net.bytebuddy.android.test.aar.lib.SomeAarClass;
+
 import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ServiceLoader;
 
-@RunWith(AndroidJUnit4.class)
 public class ByteBuddyInstrumentedTest {
-
-    @Rule
-    public ActivityScenarioRule<MainActivity> activityRule = new ActivityScenarioRule<>(MainActivity.class);
 
     @Test
     public void bytecodeInstrumentation() {
         // On local java
-        onView(withId(R.id.text_from_local_java_class)).check(matches(withText("bar")));
+        assertEquals("bar", new SomeClass().method());
         // On dependency class
-        onView(withId(R.id.text_from_aar_dependency)).check(matches(withText("bar")));
+        assertEquals("bar", new SomeAarClass().method());
         // From aar
-        onView(withId(R.id.text_instrumented_from_aar)).check(matches(withText("bar")));
+        assertEquals("bar", new AnotherClass().method());
     }
 
     @Test
