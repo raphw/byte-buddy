1113493047f4320540bb54aa1f77b9abf3b31778
Cesar Munoz <likethesalad@gmail.com>
2023-09-28 15:24:00 +0200

    Adding SPI stability tests

--- byte-buddy-gradle-plugin/android-plugin-test/app/src/androidTest/java/net/bytebuddy/android/test/ByteBuddyInstrumentedTest.java
+++ byte-buddy-gradle-plugin/android-plugin-test/app/src/androidTest/java/net/bytebuddy/android/test/ByteBuddyInstrumentedTest.java
@@ -5,12 +5,21 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.service.definition.ServiceDefinition;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ServiceLoader;
 
 @RunWith(AndroidJUnit4.class)
 public class ByteBuddyInstrumentedTest {
@@ -26,5 +35,18 @@ public class ByteBuddyInstrumentedTest {
         onView(withId(R.id.text_from_aar_dependency)).check(matches(withText("bar")));
         // From aar
         onView(withId(R.id.text_instrumented_from_aar)).check(matches(withText("bar")));
+    }
+
+    @Test
+    public void verifySPIsAreNotMissing() {
+        List<String> values = new ArrayList<>();
+        for (ServiceDefinition serviceDefinition : ServiceLoader.load(ServiceDefinition.class)) {
+            values.add(serviceDefinition.getValue());
+        }
+
+        assertEquals(3, values.size());
+        assertTrue(values.contains("Target service impl"));
+        assertTrue(values.contains("Service implementation"));
+        assertTrue(values.contains("Service implementation2"));
     }
 }
