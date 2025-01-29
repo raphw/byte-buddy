8b789d7a14668b8010840b1df4ce2f2f61c5e5ba
Rafael Winterhalter <rafael.wth@gmail.com>
2025-01-29 19:16:49 +0100

    Add javadoc.

--- byte-buddy-dep/src/main/java/net/bytebuddy/implementation/bytecode/member/Invokedynamic.java
+++ byte-buddy-dep/src/main/java/net/bytebuddy/implementation/bytecode/member/Invokedynamic.java
@@ -9,17 +9,40 @@ import org.objectweb.asm.MethodVisitor;
 
 import java.util.List;
 
+/**
+ * A stack manipulation for an invokedynamic invocation.
+ */
 @HashCodeAndEqualsPlugin.Enhance
 public class Invokedynamic extends StackManipulation.AbstractBase {
 
+    /**
+     * The name of the invokedynamic expression.
+     */
     private final String name;
 
+    /**
+     * The type that is requested for the target.
+     */
     private final JavaConstant.MethodType type;
 
+    /**
+     * A handle to the bootstrap method.
+     */
     private final JavaConstant.MethodHandle bootstrap;
 
+    /**
+     * The constant arguments to the bootstrap method.
+     */
     private final List<? extends JavaConstant> arguments;
 
+    /**
+     * Creates a stack manipulation for a bootstrap method.
+     *
+     * @param name      The name of the invokedynamic expression.
+     * @param type      The type that is requested for the target.
+     * @param bootstrap A handle to the bootstrap method.
+     * @param arguments The constant arguments to the bootstrap method.
+     */
     public Invokedynamic(String name,
                          JavaConstant.MethodType type,
                          JavaConstant.MethodHandle bootstrap,
