8a2d524ddb4ed95646793d5d4a221491995a0922
Rafael Winterhalter <rafael.wth@gmail.com>
2025-10-29 09:59:34 +0100

    Clean up reflection.

--- byte-buddy-dep/src/test/java/net/bytebuddy/description/module/ModuleDescriptionForLoadedModuleTest.java
+++ byte-buddy-dep/src/test/java/net/bytebuddy/description/module/ModuleDescriptionForLoadedModuleTest.java
@@ -15,7 +15,6 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Set;
 
 public class ModuleDescriptionForLoadedModuleTest extends AbstractModuleDescriptionTest {
 
@@ -28,69 +27,36 @@ public class ModuleDescriptionForLoadedModuleTest extends AbstractModuleDescript
     @Before
     public void setUp() throws Exception {
         super.setUp();
-
-        Class<?> pathClass = Class.forName("java.nio.file.Path");
-        Method toPathMethod = File.class.getMethod("toPath");
-        Object path = toPathMethod.invoke(jar);
-        Object pathArray = Array.newInstance(pathClass, 1);
-        Array.set(pathArray, 0, path);
-
-        // --- ModuleFinder.of(Path...) ---
-        Class<?> moduleFinderClass = Class.forName("java.lang.module.ModuleFinder");
-        Method ofMethod = moduleFinderClass.getMethod("of", pathArray.getClass());
-        Object finder = ofMethod.invoke(null, pathArray);
-        Object finderEmpty = ofMethod.invoke(null, Array.newInstance(pathClass, 0));
-
-        // --- finder.findAll().stream().findFirst() ---
-        Method findAllMethod = moduleFinderClass.getMethod("findAll");
-        Collection<?> refs = (Collection<?>) findAllMethod.invoke(finder);
-        Object ref = refs.iterator().next();
-
-        // --- ref.descriptor() ---
-        Method descriptorMethod = ref.getClass().getMethod("descriptor");
-        Object descriptor = descriptorMethod.invoke(ref);
-
-        // --- descriptor.name() ---
-        Method nameMethod = descriptor.getClass().getMethod("name");
-        String moduleName = (String) nameMethod.invoke(descriptor);
-
+        Class<?> path = Class.forName("java.nio.file.Path");
+        Class<?> moduleFinder = Class.forName("java.lang.module.ModuleFinder");
+        Class<?> moduleLayer = Class.forName("java.lang.ModuleLayer");
+        Object paths = Array.newInstance(path, 1);
+        Array.set(paths, 0, File.class.getMethod("toPath").invoke(jar));
+        Method moduleFinderOf = moduleFinder.getMethod("of", paths.getClass());
+        Object finder = moduleFinderOf.invoke(null, paths);
+        Object reference = ((Collection<?>) moduleFinder.getMethod("findAll").invoke(finder)).iterator().next();
+        Object descriptor = reference.getClass().getMethod("descriptor").invoke(reference);
+        String name = (String) descriptor.getClass().getMethod("name").invoke(descriptor);
         classLoader = new URLClassLoader(
                 new URL[]{jar.toURI().toURL()},
                 ClassLoader.getSystemClassLoader().getParent()
         );
-        // --- ModuleLayer.boot() ---
-        Class<?> moduleLayerClass = Class.forName("java.lang.ModuleLayer");
-        Method bootMethod = moduleLayerClass.getMethod("boot");
-        Object parentLayer = bootMethod.invoke(null);
-
-        // --- parentLayer.configuration() ---
-        Method configMethod = parentLayer.getClass().getMethod("configuration");
-        Object parentConfig = configMethod.invoke(parentLayer);
-
-        // --- Configuration.resolve(...) ---
+        Object parent = moduleLayer.getMethod("boot").invoke(null);
         Class<?> configClass = Class.forName("java.lang.module.Configuration");
-        Method resolveMethod = configClass.getMethod(
+        Object config = configClass.getMethod(
                 "resolve",
-                moduleFinderClass,
-                moduleFinderClass,
-                Collection.class);
-        Set<String> roots = Collections.singleton(moduleName);
-        Object config = resolveMethod.invoke(parentConfig, finder, finderEmpty, roots);
-
-        // --- parentLayer.defineModulesWithOneLoader(...) ---
-        Method defineMethod = moduleLayerClass.getMethod(
-                "defineModulesWithOneLoader",
-                configClass,
-                ClassLoader.class
-        );
-        Object layer = defineMethod.invoke(parentLayer, config, classLoader);
-
-        // --- layer.findModule(...) ---
-        Method findModuleMethod = moduleLayerClass.getMethod("findModule", String.class);
-        Object moduleOpt = findModuleMethod.invoke(layer, moduleName);
-        Class<?> optionalClass = Class.forName("java.util.Optional");
-        Method getMethod = optionalClass.getMethod("orElseThrow");
-        module = getMethod.invoke(moduleOpt);
+                moduleFinder,
+                moduleFinder,
+                Collection.class).invoke(parent.getClass().getMethod("configuration").invoke(parent),
+                finder,
+                moduleFinderOf.invoke(null, Array.newInstance(path, 0)),
+                Collections.singleton(name));
+        module = Class.forName("java.util.Optional")
+                .getMethod("orElseThrow")
+                .invoke(moduleLayer.getMethod("findModule", String.class).invoke(moduleLayer.getMethod(
+                    "defineModulesWithOneLoader",
+                    configClass,
+                    ClassLoader.class).invoke(parent, config, classLoader), name));
     }
 
     @After
